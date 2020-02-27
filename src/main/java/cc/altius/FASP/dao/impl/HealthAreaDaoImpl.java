256069bfbbd0c0492b0e9b422a9b7167803f0c0f
Akil Mahimwala <akil.m@altius.cc>
2020-02-27 17:15:33 +0530

    Updated the HealthArea Crud to include RealmCountry List

--- src/main/java/cc/altius/FASP/dao/impl/HealthAreaDaoImpl.java
+++ src/main/java/cc/altius/FASP/dao/impl/HealthAreaDaoImpl.java
@@ -8,7 +8,8 @@ package cc.altius.FASP.dao.impl;
 import cc.altius.FASP.dao.HealthAreaDao;
 import cc.altius.FASP.dao.LabelDao;
 import cc.altius.FASP.model.HealthArea;
-import cc.altius.FASP.model.rowMapper.HealthAreaRowMapper;
+import cc.altius.FASP.model.rowMapper.HealthAreaListResultSetExtractor;
+import cc.altius.FASP.model.rowMapper.HealthAreaResultSetExtractor;
 import cc.altius.utils.DateUtils;
 import java.util.Date;
 import java.util.HashMap;
@@ -17,7 +18,9 @@ import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -55,49 +58,96 @@ public class HealthAreaDaoImpl implements HealthAreaDao {
         params.put("CREATED_DATE", curDate);
         params.put("LAST_MODIFIED_BY", curUser);
         params.put("LAST_MODIFIED_DATE", curDate);
-        return si.executeAndReturnKey(params).intValue();
+        int healthAreaId = si.executeAndReturnKey(params).intValue();
+        si = new SimpleJdbcInsert(jdbcTemplate).withTableName("rm_health_area_country");
+        SqlParameterSource[] paramList = new SqlParameterSource[h.getRealmCountryArray().length];
+        int i = 0;
+        for (int realmCountryId : h.getRealmCountryArray()) {
+            params = new HashMap<>();
+            params.put("HEALTH_AREA_ID", healthAreaId);
+            params.put("REALM_COUNTRY_ID", realmCountryId);
+            params.put("CREATED_BY", curUser);
+            params.put("CREATED_DATE", curDate);
+            params.put("LAST_MODIFIED_BY", curUser);
+            params.put("LAST_MODIFIED_DATE", curDate);
+            params.put("ACTIVE", true);
+            paramList[i] = new MapSqlParameterSource(params);
+            i++;
+        }
+        si.executeBatch(paramList);
+        return healthAreaId;
     }
 
     @Override
+    @Transactional
     public int updateHealthArea(HealthArea h, int curUser) {
+        Date curDate = DateUtils.getCurrentDateObject(DateUtils.EST);
         Map<String, Object> params = new HashMap<>();
         params.put("healthAreaId", h.getHealthAreaId());
         params.put("active", h.isActive());
         params.put("curUser", curUser);
-        params.put("curDate", DateUtils.getCurrentDateObject(DateUtils.EST));
+        params.put("curDate", curDate);
         NamedParameterJdbcTemplate nm = new NamedParameterJdbcTemplate(this.jdbcTemplate);
-        return nm.update("UPDATE rm_health_area ha SET ha.ACTIVE=:active, ha.LAST_MODIFIED_BY=:curUser, ha.LAST_MODIFIED_DATE=:curDate WHERE ha.HEALTH_AREA_ID=:healthAreaId", params);
+        int rows = nm.update("UPDATE rm_health_area ha SET ha.ACTIVE=:active, ha.LAST_MODIFIED_BY=:curUser, ha.LAST_MODIFIED_DATE=:curDate WHERE ha.HEALTH_AREA_ID=:healthAreaId", params);
+        this.jdbcTemplate.update("DELETE FROM rm_health_area_country WHERE HEALTH_AREA_ID=?", h.getHealthAreaId());
+        SimpleJdbcInsert si = new SimpleJdbcInsert(jdbcTemplate).withTableName("rm_health_area_country");
+        SqlParameterSource[] paramList = new SqlParameterSource[h.getRealmCountryArray().length];
+        int i = 0;
+        for (int realmCountryId : h.getRealmCountryArray()) {
+            params = new HashMap<>();
+            params.put("HEALTH_AREA_ID", h.getHealthAreaId());
+            params.put("REALM_COUNTRY_ID", realmCountryId);
+            params.put("CREATED_BY", curUser);
+            params.put("CREATED_DATE", curDate);
+            params.put("LAST_MODIFIED_BY", curUser);
+            params.put("LAST_MODIFIED_DATE", curDate);
+            params.put("ACTIVE", true);
+            paramList[i] = new MapSqlParameterSource(params);
+            i++;
+        }
+        si.executeBatch(paramList);
+        return rows;
     }
 
     @Override
     public List<HealthArea> getHealthAreaList() {
         String sqlString = "SELECT "
                 + "	ha.HEALTH_AREA_ID, hal.LABEL_ID, hal.LABEL_EN, hal.LABEL_FR, hal.LABEL_SP, hal.LABEL_PR, "
-                + "    r.REALM_ID, r.REALM_CODE, rl.LABEL_ID `REALM_LABEL_ID`, rl.LABEL_EN `REALM_LABEL_EN`, rl.LABEL_FR `REALM_LABEL_FR`, rl.LABEL_SP `REALM_LABEL_SP`, rl.LABEL_PR `REALM_LABEL_PR`, "
-                + "	ha.ACTIVE, cb.USER_ID `CB_USER_ID`, cb.USERNAME `CB_USERNAME`, ha.CREATED_DATE, lmb.USER_ID `LMB_USER_ID`, lmb.USERNAME `LMB_USERNAME`, ha.LAST_MODIFIED_DATE "
-                + "FROM rm_health_area ha "
-                + "LEFT JOIN rm_realm r ON ha.REALM_ID=r.REALM_ID "
-                + "LEFT JOIN ap_label hal ON ha.LABEL_ID=hal.LABEL_ID "
-                + "LEFT JOIN ap_label rl ON r.LABEL_ID=rl.LABEL_ID "
-                + "LEFT JOIN us_user cb ON ha.CREATED_BY=cb.USER_ID "
-                + "LEFT JOIN us_user lmb ON ha.LAST_MODIFIED_BY=lmb.USER_ID";
-        return this.jdbcTemplate.query(sqlString, new HealthAreaRowMapper());
-    }
-
-    @Override
-    public HealthArea getHealthAreaById(int healthAreaId) {
-        String sqlString = "SELECT "
-                + "	ha.HEALTH_AREA_ID, hal.LABEL_ID, hal.LABEL_EN, hal.LABEL_FR, hal.LABEL_SP, hal.LABEL_PR, "
-                + "    r.REALM_ID, r.REALM_CODE, rl.LABEL_ID `REALM_LABEL_ID`, rl.LABEL_EN `REALM_LABEL_EN`, rl.LABEL_FR `REALM_LABEL_FR`, rl.LABEL_SP `REALM_LABEL_SP`, rl.LABEL_PR `REALM_LABEL_PR`, "
-                + "	ha.ACTIVE, cb.USER_ID `CB_USER_ID`, cb.USERNAME `CB_USERNAME`, ha.CREATED_DATE, lmb.USER_ID `LMB_USER_ID`, lmb.USERNAME `LMB_USERNAME`, ha.LAST_MODIFIED_DATE "
+                + "     r.REALM_ID, r.REALM_CODE, rl.LABEL_ID `REALM_LABEL_ID`, rl.LABEL_EN `REALM_LABEL_EN`, rl.LABEL_FR `REALM_LABEL_FR`, rl.LABEL_SP `REALM_LABEL_SP`, rl.LABEL_PR `REALM_LABEL_PR`, "
+                + "	ha.ACTIVE, cb.USER_ID `CB_USER_ID`, cb.USERNAME `CB_USERNAME`, ha.CREATED_DATE, lmb.USER_ID `LMB_USER_ID`, lmb.USERNAME `LMB_USERNAME`, ha.LAST_MODIFIED_DATE, "
+                + "     rc.REALM_COUNTRY_ID, rc.COUNTRY_ID, cl.LABEL_ID `COUNTRY_LABEL_ID`, cl.LABEL_EN `COUNTRY_LABEL_EN`, cl.LABEL_FR `COUNTRY_LABEL_FR`, cl.LABEL_SP `COUNTRY_LABEL_SP`, cl.LABEL_PR `COUNTRY_LABEL_PR` "
                 + "FROM rm_health_area ha "
                 + "LEFT JOIN rm_realm r ON ha.REALM_ID=r.REALM_ID "
                 + "LEFT JOIN ap_label hal ON ha.LABEL_ID=hal.LABEL_ID "
                 + "LEFT JOIN ap_label rl ON r.LABEL_ID=rl.LABEL_ID "
                 + "LEFT JOIN us_user cb ON ha.CREATED_BY=cb.USER_ID "
                 + "LEFT JOIN us_user lmb ON ha.LAST_MODIFIED_BY=lmb.USER_ID "
+                + "LEFT JOIN rm_health_area_country hac ON ha.HEALTH_AREA_ID=hac.HEALTH_AREA_ID "
+                + "LEFT JOIN rm_realm_country rc ON hac.REALM_COUNTRY_ID=rc.REALM_COUNTRY_ID "
+                + "LEFT JOIN ap_country c ON rc.COUNTRY_ID=c.COUNTRY_ID "
+                + "LEFT JOIN ap_label cl ON c.LABEL_ID=cl.LABEL_ID ";
+        return this.jdbcTemplate.query(sqlString, new HealthAreaListResultSetExtractor());
+    }
+
+    @Override
+    public HealthArea getHealthAreaById(int healthAreaId) {
+        String sqlString = "SELECT "
+                + "	ha.HEALTH_AREA_ID, hal.LABEL_ID, hal.LABEL_EN, hal.LABEL_FR, hal.LABEL_SP, hal.LABEL_PR, "
+                + "     r.REALM_ID, r.REALM_CODE, rl.LABEL_ID `REALM_LABEL_ID`, rl.LABEL_EN `REALM_LABEL_EN`, rl.LABEL_FR `REALM_LABEL_FR`, rl.LABEL_SP `REALM_LABEL_SP`, rl.LABEL_PR `REALM_LABEL_PR`, "
+                + "	ha.ACTIVE, cb.USER_ID `CB_USER_ID`, cb.USERNAME `CB_USERNAME`, ha.CREATED_DATE, lmb.USER_ID `LMB_USER_ID`, lmb.USERNAME `LMB_USERNAME`, ha.LAST_MODIFIED_DATE, "
+                + "     rc.REALM_COUNTRY_ID, rc.COUNTRY_ID, cl.LABEL_ID `COUNTRY_LABEL_ID`, cl.LABEL_EN `COUNTRY_LABEL_EN`, cl.LABEL_FR `COUNTRY_LABEL_FR`, cl.LABEL_SP `COUNTRY_LABEL_SP`, cl.LABEL_PR `COUNTRY_LABEL_PR` "
+                + "FROM rm_health_area ha "
+                + "LEFT JOIN rm_realm r ON ha.REALM_ID=r.REALM_ID "
+                + "LEFT JOIN ap_label hal ON ha.LABEL_ID=hal.LABEL_ID "
+                + "LEFT JOIN ap_label rl ON r.LABEL_ID=rl.LABEL_ID "
+                + "LEFT JOIN us_user cb ON ha.CREATED_BY=cb.USER_ID "
+                + "LEFT JOIN us_user lmb ON ha.LAST_MODIFIED_BY=lmb.USER_ID "
+                + "LEFT JOIN rm_health_area_country hac ON ha.HEALTH_AREA_ID=hac.HEALTH_AREA_ID "
+                + "LEFT JOIN rm_realm_country rc ON hac.REALM_COUNTRY_ID=rc.REALM_COUNTRY_ID "
+                + "LEFT JOIN ap_country c ON rc.COUNTRY_ID=c.COUNTRY_ID "
+                + "LEFT JOIN ap_label cl ON c.LABEL_ID=cl.LABEL_ID "
                 + "WHERE ha.HEALTH_AREA_ID=?";
-        return this.jdbcTemplate.queryForObject(sqlString, new HealthAreaRowMapper(), healthAreaId);
+        return this.jdbcTemplate.query(sqlString, new HealthAreaResultSetExtractor(), healthAreaId);
     }
 
 }
