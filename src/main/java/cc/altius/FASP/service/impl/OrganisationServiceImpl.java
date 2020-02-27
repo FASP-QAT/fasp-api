122d86a48329328e77ec3d0feee465717d77606b
Vivek <vivek.g@altius.cc>
2020-02-27 11:50:25 +0530

    sdf

---
+++ src/main/java/cc/altius/FASP/service/impl/OrganisationServiceImpl.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc.altius.FASP.service.impl;
+
+import cc.altius.FASP.dao.OrganisationDao;
+import cc.altius.FASP.model.Organisation;
+import cc.altius.FASP.service.OrganisationService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author altius
+ */
+@Service
+public class OrganisationServiceImpl implements OrganisationService{
+    
+    @Autowired
+    private OrganisationDao organisationDao;
+    
+    @Override
+    public int addOrganisation(Organisation organisation, int curUser) {
+        return organisationDao.addOrganisation(organisation, curUser);
+    }
+
+    @Override
+    public int updateOrganisation(Organisation organisation, int curUser) {
+        return organisationDao.updateOrganisation(organisation,curUser);
+    }
+
+    @Override
+    public List<Organisation> getOrganisationList() {
+        return organisationDao.getOrganisationList();
+    }
+
+    @Override
+    public Organisation getOrganisationById(int organisationId) {
+        return organisationDao.getOrganisationById(organisationId);
+    }
+    
+}
