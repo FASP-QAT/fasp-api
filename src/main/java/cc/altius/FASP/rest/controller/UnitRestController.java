482d658964aba1b00c0895fb1653077f04285b51
palashnikam <nikampalash@gmail.com>
2020-03-03 15:14:51 +0530

    palash commit

--- src/main/java/cc/altius/FASP/rest/controller/UnitRestController.java
+++ src/main/java/cc/altius/FASP/rest/controller/UnitRestController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author akil
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4202")
+@CrossOrigin(origins = {"http://localhost:4202", "https://faspdeveloper.github.io"})
 public class UnitRestController {
 
     @Autowired
