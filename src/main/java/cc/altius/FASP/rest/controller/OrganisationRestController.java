efcf026ccac03f95eb2c45b8c9a4cd0595f2326e
Anchal Chauhan <anchal.c@altius.cc>
2020-03-04 11:58:14 +0530

    Commited by anchal

--- src/main/java/cc/altius/FASP/rest/controller/OrganisationRestController.java
+++ src/main/java/cc/altius/FASP/rest/controller/OrganisationRestController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author altius
  */
 @RestController
-@RequestMapping("/api")
 @CrossOrigin(origins = {"http://localhost:4202", "https://faspdeveloper.github.io", "chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop"})
 public class OrganisationRestController {
 
