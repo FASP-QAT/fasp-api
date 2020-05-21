e2dc24e4312991c8f26ada2018dbe4ca0b383b33
Akil Mahimwala <akil.m@altius.cc>
2020-05-21 17:42:31 +0530

    Global Consumption report

--- src/main/java/cc/altius/FASP/model/rowMapper/ForecastingUnitRowMapper.java
+++ src/main/java/cc/altius/FASP/model/rowMapper/ForecastingUnitRowMapper.java
@@ -6,11 +6,8 @@
 package cc.altius.FASP.model.rowMapper;
 
 import cc.altius.FASP.model.ForecastingUnit;
-import cc.altius.FASP.model.ProductCategory;
-import cc.altius.FASP.model.Realm;
 import cc.altius.FASP.model.SimpleCodeObject;
 import cc.altius.FASP.model.SimpleObject;
-import cc.altius.FASP.model.TracerCategory;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
@@ -28,6 +25,7 @@ public class ForecastingUnitRowMapper implements RowMapper<ForecastingUnit> {
                 new SimpleCodeObject(rs.getInt("REALM_ID"), new LabelRowMapper("REALM_").mapRow(rs, rowNum), rs.getString("REALM_CODE")),
                 new LabelRowMapper("GENERIC_").mapRow(rs, rowNum),
                 new LabelRowMapper().mapRow(rs, rowNum),
+                new SimpleCodeObject(rs.getInt("UNIT_ID"), new LabelRowMapper("UNIT_").mapRow(rs, rowNum), rs.getString("UNIT_CODE")),
                 new SimpleObject(rs.getInt("PRODUCT_CATEGORY_ID"), new LabelRowMapper("PRODUCT_CATEGORY_").mapRow(rs, rowNum)),
                 new SimpleObject(rs.getInt("TRACER_CATEGORY_ID"), new LabelRowMapper("TRACER_CATEGORY_").mapRow(rs, rowNum))
         );
