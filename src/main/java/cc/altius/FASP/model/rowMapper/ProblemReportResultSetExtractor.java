6881aa68367ee1265f5bd8c143803ec98b289eea
Akil Mahimwala <akil.m@altius.cc>
2020-07-29 16:12:06 +0530

    Updated the structure for Problem report

--- src/main/java/cc/altius/FASP/model/rowMapper/ProblemReportResultSetExtractor.java
+++ src/main/java/cc/altius/FASP/model/rowMapper/ProblemReportResultSetExtractor.java
@@ -31,16 +31,26 @@ public class ProblemReportResultSetExtractor implements ResultSetExtractor<List<
             pr.setProblemReportId(rs.getInt("PROBLEM_REPORT_ID"));
             int idx = prList.indexOf(pr);
             if (idx == -1) {
-                pr.setProgram(new SimpleCodeObject(rs.getInt("PROGRAM_ID"), new LabelRowMapper("PROGRAM_").mapRow(rs, idx), rs.getString("PROGRAM_CODE")));
+                pr.setProgram(new SimpleCodeObject(rs.getInt("PROGRAM_ID"), new LabelRowMapper("PROGRAM_").mapRow(rs, 1), rs.getString("PROGRAM_CODE")));
                 pr.setVersionId(rs.getInt("VERSION_ID"));
-                pr.setData1(rs.getString("DATA1"));
-                pr.setData2(rs.getString("DATA2"));
-                pr.setData3(rs.getString("DATA3"));
-                pr.setData4(rs.getString("DATA4"));
+                pr.setDt(rs.getString("DT")); // Date
+                pr.setRegion(new SimpleObject(rs.getInt("REGION_ID"), new LabelRowMapper("REGION_").mapRow(rs, 1))); // Region
+                if (pr.getRegion().getId() == 0) {
+                    pr.getRegion().setId(null);
+                }
+                pr.setPlanningUnit(new SimpleObject(rs.getInt("PLANNING_UNIT_ID"), new LabelRowMapper("PLANNING_UNIT_").mapRow(rs, 1))); // Planning Unit
+
+                if (pr.getPlanningUnit().getId() == 0) {
+                    pr.getPlanningUnit().setId(null);
+                }
+                pr.setShipmentId(rs.getInt("SHIPMENT_ID")); // Shipment Id
+                if (rs.wasNull()) {
+                    pr.setShipmentId(null);
+                }
                 pr.setData5(rs.getString("DATA5"));
-                pr.setProblemStatus(new SimpleObject(rs.getInt("PROBLEM_STATUS_ID"), new LabelRowMapper("PROBLEM_STATUS_").mapRow(rs, idx)));
-                pr.setProblemType(new SimpleObject(rs.getInt("PROBLEM_TYPE_ID"), new LabelRowMapper("PROBLEM_TYPE_").mapRow(rs, idx)));
-                pr.setRealmProblem(new RealmProblemRowMapper("RP_", true).mapRow(rs, idx));
+                pr.setProblemStatus(new SimpleObject(rs.getInt("PROBLEM_STATUS_ID"), new LabelRowMapper("PROBLEM_STATUS_").mapRow(rs, 1)));
+                pr.setProblemType(new SimpleObject(rs.getInt("PROBLEM_TYPE_ID"), new LabelRowMapper("PROBLEM_TYPE_").mapRow(rs, 1)));
+                pr.setRealmProblem(new RealmProblemRowMapper("RP_", true).mapRow(rs, 1));
                 pr.setCreatedBy(new BasicUser(rs.getInt("CB_USER_ID"), rs.getString("CB_USERNAME")));
                 pr.setCreatedDate(rs.getTimestamp("CREATED_DATE"));
                 pr.setLastModifiedBy(new BasicUser(rs.getInt("LMB_USER_ID"), rs.getString("LMB_USERNAME")));
@@ -54,7 +64,7 @@ public class ProblemReportResultSetExtractor implements ResultSetExtractor<List<
             prt.setNotes(rs.getString("NOTES"));
             prt.setCreatedBy(new BasicUser(rs.getInt("CBT_USER_ID"), rs.getString("CBT_USERNAME")));
             prt.setCreatedDate(rs.getTimestamp("TRANS_CREATED_DATE"));
-            prt.setProblemStatus(new SimpleObject(rs.getInt("PROBLEM_STATUS_TRANS_ID"), new LabelRowMapper("PROBLEM_STATUS_TRANS_").mapRow(rs, idx)));
+            prt.setProblemStatus(new SimpleObject(rs.getInt("PROBLEM_STATUS_TRANS_ID"), new LabelRowMapper("PROBLEM_STATUS_TRANS_").mapRow(rs, 1)));
             pr.getProblemTransList().add(prt);
         }
         return prList;
