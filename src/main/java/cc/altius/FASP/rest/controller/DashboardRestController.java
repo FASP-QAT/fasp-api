cfe19cf0b05caf0ab3907fe4a7cc6bd56c29df0f
anchal-chouvhan-altius <anchal.c@altius.cc>
2020-07-29 21:58:14 +0530

    Commited by anchal

--- src/main/java/cc/altius/FASP/rest/controller/DashboardRestController.java
+++ src/main/java/cc/altius/FASP/rest/controller/DashboardRestController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,11 +43,34 @@ public class DashboardRestController {
         }
     }
 
-    @GetMapping(value = "/realmLevelDashboard")
-    public ResponseEntity realmLevelDashboard(Authentication auth) {
+    @GetMapping(value = "/realmLevelDashboard/{realmId}")
+    public ResponseEntity realmLevelDashboard(@PathVariable("realmId") int realmId,Authentication auth) {
         try {
             CustomUserDetails curUser = (CustomUserDetails) auth.getPrincipal();
-            return new ResponseEntity(this.dashboardService.getRealmLevelDashboard(), HttpStatus.OK);
+            return new ResponseEntity(this.dashboardService.getRealmLevelDashboard(realmId), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error while getting country list", e);
+            return new ResponseEntity(new ResponseCode("static.message.listFailed"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/applicationLevelDashboardUserList")
+    public ResponseEntity applicationLevelDashboardUserList(Authentication auth) {
+        try {
+            CustomUserDetails curUser = (CustomUserDetails) auth.getPrincipal();
+            return new ResponseEntity(this.dashboardService.getUserListForApplicationLevelAdmin(), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error while getting country list", e);
+            return new ResponseEntity(new ResponseCode("static.message.listFailed"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/realmLevelDashboardUserList/{realmId}")
+    public ResponseEntity realmLevelDashboardUserList(@PathVariable("realmId") int realmId,Authentication auth) {
+        try {
+            CustomUserDetails curUser = (CustomUserDetails) auth.getPrincipal();
+            System.out.println("users---"+this.dashboardService.getUserListForRealmLevelAdmin(realmId));
+            return new ResponseEntity(this.dashboardService.getUserListForRealmLevelAdmin(realmId), HttpStatus.OK);
         } catch (Exception e) {
             logger.error("Error while getting country list", e);
             return new ResponseEntity(new ResponseCode("static.message.listFailed"), HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/cc/altius/FASP/service/impl/DashboardServiceImpl.java
+++ src/main/java/cc/altius/FASP/service/impl/DashboardServiceImpl.java
@@ -7,6 +7,7 @@ package cc.altius.FASP.service.impl;
 
 import cc.altius.FASP.dao.DashboardDao;
 import cc.altius.FASP.service.DashboardService;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,8 +28,18 @@ public class DashboardServiceImpl implements DashboardService {
     }
 
     @Override
-    public Map<String, Object> getRealmLevelDashboard() {
-        return this.dashboardDao.getRealmLevelDashboard();
+    public Map<String, Object> getRealmLevelDashboard(int realmId) {
+        return this.dashboardDao.getRealmLevelDashboard(realmId);
+    }
+
+    @Override
+    public List<Map<String, Object>> getUserListForApplicationLevelAdmin() {
+        return this.dashboardDao.getUserListForApplicationLevelAdmin();
+    }
+
+    @Override
+    public List<Map<String, Object>> getUserListForRealmLevelAdmin(int realmId) {
+        return this.dashboardDao.getUserListForRealmLevelAdmin(realmId);
     }
 
 }
