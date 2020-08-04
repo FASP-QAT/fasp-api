962e17b46202c2a336938efa98e505bcc8beb687
anchal-chouvhan-altius <anchal.c@altius.cc>
2020-08-04 19:33:15 +0530

    Commited by anchal

--- src/main/java/cc/altius/FASP/rest/controller/UserRestController.java
+++ src/main/java/cc/altius/FASP/rest/controller/UserRestController.java
@@ -177,6 +177,7 @@ public class UserRestController {
     @GetMapping(value = "/user/{userId}")
     public ResponseEntity getUserByUserId(@PathVariable int userId) {
         try {
+            auditLogger.info("userId " + userId);
             return new ResponseEntity(this.userService.getUserByUserId(userId), HttpStatus.OK);
         } catch (AccessDeniedException e) {
             logger.error(("Could not get User list for UserId=" + userId));
@@ -186,6 +187,7 @@ public class UserRestController {
             return new ResponseEntity(new ResponseCode("static.message.listFailed"), HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             logger.error(("Could not get User list for UserId=" + userId));
+            auditLogger.info("Could not get User list for UserId=" + userId);
             return new ResponseEntity(new ResponseCode("static.message.listFailed"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -500,15 +502,19 @@ public class UserRestController {
     @PostMapping("/user/agreement")
     public ResponseEntity acceptUserAgreement(Authentication auth) {
         try {
-            CustomUserDetails curUser = this.userService.getCustomUserByUserId(((CustomUserDetails) auth.getPrincipal()).getUserId());
-            System.out.println("cur user---"+curUser);
-//            auditLogger.info("Update agreement for Username: " + curUser.getUsername());
-            this.userService.acceptUserAgreement(curUser.getUserId());
+//            CustomUserDetails curUser = this.userService.getCustomUserByUserId(((CustomUserDetails) auth.getPrincipal()).getUserId());
+//            System.out.println("cur user---"+curUser);
+            auditLogger.info("auth 1: " + (CustomUserDetails) auth.getPrincipal());
+            auditLogger.info("auth 2: " + auth);
+            auditLogger.info("auth 3: " + ((CustomUserDetails) auth.getPrincipal()).getUserId());
+//            auditLogger.info("Update agreement for Username: " + curUser);
+            this.userService.acceptUserAgreement(((CustomUserDetails) auth.getPrincipal()).getUserId());
 //            auditLogger.info("Agreement updated successfully for Username: " + curUser.getUsername());
             return new ResponseEntity("", HttpStatus.OK);
         } catch (Exception e) {
             auditLogger.info("Could not update agreement", e);
             return new ResponseEntity(new ResponseCode("static.message.user.languageChangeError"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
     }
 }
