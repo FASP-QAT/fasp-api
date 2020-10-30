6509e01590479a6c834affa7f60ff35a7d7ad911
anchal-chouvhan-altius <anchal.c@altius.cc>
2020-10-30 15:11:18 +0530

    Commited by anchal

---
+++ src/main/java/cc/altius/FASP/model/DashboardUser.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc.altius.FASP.model;
+
+import cc.altius.FASP.model.Label;
+
+/**
+ *
+ * @author altius
+ */
+public class DashboardUser {
+
+    private Label label;
+    private int count;
+
+    public Label getLabel() {
+        return label;
+    }
+
+    public void setLabel(Label label) {
+        this.label = label;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "DashboardUser{" + "label=" + label + ", count=" + count + '}';
+    }
+
+}
