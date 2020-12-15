d3051c3f8d94cc2a0447bd7d35d5fe2fa2b3c136
Ekta <ekta.n@altius.cc>
2020-12-15 10:04:02 +0000

    Removed the files that were not required

--- dbRestore.sh
+++ dbRestore.sh
@@ -3,4 +3,3 @@ sed -i 's/localhost/%/g' fasp-db.sql
 sed -i 's/root/faspUser/g' fasp-db.sql
 mysql -u faspUser -p -h qatuat.ckjjt1wn5lh7.us-east-1.rds.amazonaws.com fasp < fasp-db.sql
 rm fasp-db.sql
-
