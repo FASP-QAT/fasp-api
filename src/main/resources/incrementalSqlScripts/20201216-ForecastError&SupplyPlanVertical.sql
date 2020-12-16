c0532b9b744cb25768337e69311189c9b0accdc9
Akil Mahimwala <akil.m@altius.cc>
2020-12-16 15:40:37 +0530

    Fix for ForecastMetrics report and Stock Status Vertical report

---
+++ src/main/resources/incrementalSqlScripts/20201216-ForecastError&SupplyPlanVertical.sql
@@ -0,0 +1,192 @@
+
+CREATE DEFINER=`faspUser`@`%` PROCEDURE `stockStatusReportVertical`(VAR_START_DATE DATE, VAR_STOP_DATE DATE, VAR_PROGRAM_ID INT(10), VAR_VERSION_ID INT, VAR_PLANNING_UNIT_ID INT(10))
+BEGIN
+	-- %%%%%%%%%%%%%%%%%%%%%
+        -- Report no 16
+	-- %%%%%%%%%%%%%%%%%%%%%
+	
+        SET @startDate = VAR_START_DATE;
+	SET @stopDate = VAR_STOP_DATE;
+	SET @programId = VAR_PROGRAM_ID;
+	SET @versionId = VAR_VERSION_ID;
+	SET @planningUnitId = VAR_PLANNING_UNIT_ID;
+
+	IF @versionId = -1 THEN
+		SELECT MAX(pv.VERSION_ID) INTO @versionId FROM rm_program_version pv WHERE pv.PROGRAM_ID=@programId;
+	END IF;
+    
+	SELECT 
+        mn.MONTH `TRANS_DATE`, 
+        sma.OPENING_BALANCE `FINAL_OPENING_BALANCE`, 
+        sma.ACTUAL_CONSUMPTION_QTY, sma.FORECASTED_CONSUMPTION_QTY, 
+        sma.ACTUAL,
+        sma.SHIPMENT_QTY SQTY,
+        sma.ADJUSTMENT_MULTIPLIED_QTY `ADJUSTMENT`,
+        sma.EXPIRED_STOCK,
+        sma.CLOSING_BALANCE `FINAL_CLOSING_BALANCE`,
+        sma.AMC,
+        sma.MOS `MoS`,
+        sma.MIN_STOCK_MOS `MIN_MONTHS_OF_STOCK`,
+        sma.MAX_STOCK_MOS `MAX_MONTHS_OF_STOCK`,
+        sh.SHIPMENT_ID, sh.SHIPMENT_QTY, 
+        fs.FUNDING_SOURCE_ID, fs.FUNDING_SOURCE_CODE, fs.LABEL_ID `FUNDING_SOURCE_LABEL_ID`, fs.LABEL_EN `FUNDING_SOURCE_LABEL_EN`, fs.LABEL_FR `FUNDING_SOURCE_LABEL_FR`, fs.LABEL_SP `FUNDING_SOURCE_LABEL_SP`, fs.LABEL_PR `FUNDING_SOURCE_LABEL_PR`, 
+        pa.PROCUREMENT_AGENT_ID, pa.PROCUREMENT_AGENT_CODE, pa.LABEL_ID `PROCUREMENT_AGENT_LABEL_ID`, pa.LABEL_EN `PROCUREMENT_AGENT_LABEL_EN`, pa.LABEL_FR `PROCUREMENT_AGENT_LABEL_FR`, pa.LABEL_SP `PROCUREMENT_AGENT_LABEL_SP`, pa.LABEL_PR `PROCUREMENT_AGENT_LABEL_PR`, 
+        ss.SHIPMENT_STATUS_ID, ss.LABEL_ID `SHIPMENT_STATUS_LABEL_ID`, ss.LABEL_EN `SHIPMENT_STATUS_LABEL_EN`, ss.LABEL_Fr `SHIPMENT_STATUS_LABEL_FR`, ss.LABEL_SP `SHIPMENT_STATUS_LABEL_SP`, ss.LABEL_PR `SHIPMENT_STATUS_LABEL_PR`
+    FROM
+        mn 
+        LEFT JOIN rm_supply_plan_amc sma ON 
+            mn.MONTH=sma.TRANS_DATE 
+            AND sma.PROGRAM_ID = @programId
+            AND sma.VERSION_ID = @versionId
+            AND sma.PLANNING_UNIT_ID = @planningUnitId
+        LEFT JOIN 
+            (
+            SELECT COALESCE(st.RECEIVED_DATE, st.EXPECTED_DELIVERY_DATE) `EDD`, s.SHIPMENT_ID, st.SHIPMENT_QTY , st.FUNDING_SOURCE_ID, st.PROCUREMENT_AGENT_ID, st.SHIPMENT_STATUS_ID
+            FROM 
+                (
+                SELECT s.SHIPMENT_ID, MAX(st.VERSION_ID) MAX_VERSION_ID FROM rm_shipment s LEFT JOIN rm_shipment_trans st ON s.SHIPMENT_ID=st.SHIPMENT_ID WHERE s.PROGRAM_ID=@programId AND st.VERSION_ID<=@versionId AND st.SHIPMENT_TRANS_ID IS NOT NULL GROUP BY s.SHIPMENT_ID 
+            ) AS s 
+            LEFT JOIN rm_shipment_trans st ON s.SHIPMENT_ID=st.SHIPMENT_ID AND s.MAX_VERSION_ID=st.VERSION_ID 
+            WHERE 
+                st.ACTIVE 
+                AND st.SHIPMENT_STATUS_ID != 8 
+                AND st.ACCOUNT_FLAG
+                AND COALESCE(st.RECEIVED_DATE, st.EXPECTED_DELIVERY_DATE) BETWEEN @startDate AND @stopDate 
+                AND st.PLANNING_UNIT_ID =@planningUnitId
+        ) sh ON LEFT(sma.TRANS_DATE,7)=LEFT(sh.EDD,7)
+        LEFT JOIN vw_funding_source fs ON sh.FUNDING_SOURCE_ID=fs.FUNDING_SOURCE_ID
+        LEFT JOIN vw_procurement_agent pa ON sh.PROCUREMENT_AGENT_ID=pa.PROCUREMENT_AGENT_ID
+        LEFT JOIN vw_shipment_status ss ON sh.SHIPMENT_STATUS_ID=ss.SHIPMENT_STATUS_ID
+    WHERE
+        mn.MONTH BETWEEN @startDate AND @stopDate
+    ORDER BY mn.MONTH;
+    
+END
+
+CREATE DEFINER=`faspUser`@`%` PROCEDURE `forecastMetricsComparision`(VAR_USER_ID INT(10), VAR_REALM_ID INT(10), VAR_START_DATE DATE, VAR_REALM_COUNTRY_IDS TEXT, VAR_PROGRAM_IDS TEXT, VAR_PLANNING_UNIT_IDS TEXT, VAR_PREVIOUS_MONTHS INT(10), VAR_APPROVED_SUPPLY_PLAN_ONLY TINYINT(1))
+BEGIN
+
+	-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	-- Report no 5
+	-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+	-- realmId since it is a Global report need to include Realm
+        -- startDate - date that the report is to be run for
+        -- realmCountryIds list of countries that we need to run the report for
+        -- programIds is the list of programs that we need to run the report for
+        -- planningUnitIds is the list of planningUnits that we need to run the report for
+        -- previousMonths is the number of months that the calculation should go in the past for (excluding the current month) calculation of WAPE formulae
+        -- current month is always included in the calculation
+        -- only consider those months that have both a Forecasted and Actual consumption
+        -- WAPE Formulae
+        -- ((Abs(actual consumption month 1-forecasted consumption month 1)+ Abs(actual consumption month 2-forecasted consumption month 2)+ Abs(actual consumption month 3-forecasted consumption month 3)+ Abs(actual consumption month 4-forecasted consumption month 4)+ Abs(actual consumption month 5-forecasted consumption month 5)+ Abs(actual consumption month 6-forecasted consumption month 6)) / (Sum of all actual consumption in the last 6 months)) 
+
+	DECLARE curRealmCountryId INT;
+        DECLARE curHealthAreaId INT;
+        DECLARE curOrganisationId INT;
+        DECLARE curProgramId INT;
+	DECLARE done INT DEFAULT FALSE;
+	DECLARE cursor_acl CURSOR FOR SELECT acl.REALM_COUNTRY_ID, acl.HEALTH_AREA_ID, acl.ORGANISATION_ID, acl.PROGRAM_ID FROM us_user_acl acl WHERE acl.USER_ID=VAR_USER_ID;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+        SET @aclSqlString = CONCAT("       AND (FALSE ");
+        OPEN cursor_acl;
+	read_loop: LOOP
+        FETCH cursor_acl INTO curRealmCountryId, curHealthAreaId, curOrganisationId, curProgramId;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        -- For each loop build the sql for acl
+        SET @aclSqlString = CONCAT(@aclSqlString,"       OR (");
+        SET @aclSqlString = CONCAT(@aclSqlString,"           (p.PROGRAM_ID IS NULL OR ",IFNULL(curRealmCountryId,-1),"=-1 OR p.REALM_COUNTRY_ID=",IFNULL(curRealmCountryId,-1),")");
+        SET @aclSqlString = CONCAT(@aclSqlString,"       AND (p.PROGRAM_ID IS NULL OR ",IFNULL(curHealthAreaId,-1)  ,"=-1 OR p.HEALTH_AREA_ID="  ,IFNULL(curHealthAreaId,-1)  ,")");
+        SET @aclSqlString = CONCAT(@aclSqlString,"       AND (p.PROGRAM_ID IS NULL OR ",IFNULL(curOrganisationId,-1),"=-1 OR p.ORGANISATION_ID=" ,IFNULL(curOrganisationId,-1),")");
+        SET @aclSqlString = CONCAT(@aclSqlString,"       AND (p.PROGRAM_ID IS NULL OR ",IFNULL(curProgramId,-1)     ,"=-1 OR p.PROGRAM_ID="      ,IFNULL(curProgramId,-1)     ,")");
+        SET @aclSqlString = CONCAT(@aclSqlString,"       )");
+    END LOOP;
+    CLOSE cursor_acl;
+    SET @aclSqlString = CONCAT(@aclSqlString, ") ");
+    
+    SET @realmId = VAR_REALM_ID;
+    SET @startDate = VAR_START_DATE;
+    SET @previousMonths = VAR_PREVIOUS_MONTHS;
+    SET @approvedSupplyPlanOnly = VAR_APPROVED_SUPPLY_PLAN_ONLY;
+    
+    SET @sqlString = "";
+    
+	SET @sqlString = CONCAT(@sqlString, "SELECT ");
+    SET @sqlString = CONCAT(@sqlString, "    spa.TRANS_DATE, ");
+    SET @sqlString = CONCAT(@sqlString, "    p.PROGRAM_ID, p.PROGRAM_CODE, p.LABEL_ID `PROGRAM_LABEL_ID`, p.LABEL_EN `PROGRAM_LABEL_EN`, p.LABEL_FR `PROGRAM_LABEL_FR`, p.LABEL_SP `PROGRAM_LABEL_SP`, p.LABEL_PR `PROGRAM_LABEL_PR`, ");
+    SET @sqlString = CONCAT(@sqlString, "    pu.PLANNING_UNIT_ID, pu.LABEL_ID `PLANNING_UNIT_LABEL_ID`, pu.LABEL_EN `PLANNING_UNIT_LABEL_EN`, pu.LABEL_FR `PLANNING_UNIT_LABEL_FR`, pu.LABEL_SP `PLANNING_UNIT_LABEL_SP`, pu.LABEL_PR  `PLANNING_UNIT_LABEL_PR`, ");
+    SET @sqlString = CONCAT(@sqlString, "    spa.ACTUAL, spa.ACTUAL_CONSUMPTION_QTY `ACTUAL_CONSUMPTION`, spa.FORECASTED_CONSUMPTION_QTY `FORECASTED_CONSUMPTION`, ");
+--    SET @sqlString = CONCAT(@sqlString, "    c2.ACTUAL_CONSUMPTION_TOTAL, c2.DIFF_CONSUMPTION_TOTAL, c2.MONTH_COUNT, IF(spa.ACTUAL=1 AND spa.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND spa.FORECASTED_CONSUMPTION_QTY IS NOT NULL, c2.DIFF_CONSUMPTION_TOTAL*100/c2.ACTUAL_CONSUMPTION_TOTAL, null) FORECAST_ERROR ");
+    SET @sqlString = CONCAT(@sqlString, "    c2.ACTUAL_CONSUMPTION_TOTAL, c2.DIFF_CONSUMPTION_TOTAL, c2.MONTH_COUNT, c2.DIFF_CONSUMPTION_TOTAL*100/c2.ACTUAL_CONSUMPTION_TOTAL FORECAST_ERROR ");
+    SET @sqlString = CONCAT(@sqlString, "FROM rm_program_planning_unit ppu ");
+    SET @sqlString = CONCAT(@sqlString, "LEFT JOIN (SELECT spa.PROGRAM_ID, MAX(spa.VERSION_ID) MAX_VERSION FROM rm_supply_plan_amc spa LEFT JOIN rm_program_version pv ON spa.PROGRAM_ID=pv.PROGRAM_ID AND spa.VERSION_ID=pv.VERSION_ID ");
+    SET @sqlString = CONCAT(@sqlString, "           WHERE TRUE ");
+    IF @approvedSupplyPlanOnly THEN
+        SET @sqlString = CONCAT(@sqlString, "               AND pv.VERSION_TYPE_ID=2 AND pv.VERSION_STATUS_ID=2 ");
+    END IF;
+    IF LENGTH(VAR_PROGRAM_IDS)>0 THEN
+		SET @sqlString = CONCAT(@sqlString, "               AND spa.PROGRAM_ID IN (",VAR_PROGRAM_IDS,") ");
+    END IF;
+    SET @sqlString = CONCAT(@sqlString, "           GROUP BY spa.PROGRAM_ID ");
+    SET @sqlString = CONCAT(@sqlString, ") f ON ppu.PROGRAM_ID=f.PROGRAM_ID ");
+    SET @sqlString = CONCAT(@sqlString, "LEFT JOIN rm_supply_plan_amc spa ON spa.PROGRAM_ID=f.PROGRAM_ID AND spa.VERSION_ID=f.MAX_VERSION AND spa.TRANS_DATE=@startDate AND spa.PLANNING_UNIT_ID=ppu.PLANNING_UNIT_ID ");
+    SET @sqlString = CONCAT(@sqlString, "LEFT JOIN vw_program p ON ppu.PROGRAM_ID=p.PROGRAM_ID ");
+    SET @sqlString = CONCAT(@sqlString, "LEFT JOIN vw_planning_unit pu ON ppu.PLANNING_UNIT_ID=pu.PLANNING_UNIT_ID ");
+    SET @sqlString = CONCAT(@sqlString, "LEFT JOIN ");
+    SET @sqlString = CONCAT(@sqlString, "    ( ");
+    SET @sqlString = CONCAT(@sqlString, "    SELECT ");
+    SET @sqlString = CONCAT(@sqlString, "        @startDate `TRANS_DATE`, p.PROGRAM_ID, pu.PLANNING_UNIT_ID, ");
+    SET @sqlString = CONCAT(@sqlString, "        SUM(IF(spa.ACTUAL AND spa.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND spa.FORECASTED_CONSUMPTION_QTY IS NOT NULL, spa.ACTUAL_CONSUMPTION_QTY, null)) `ACTUAL_CONSUMPTION_TOTAL`, ");
+    SET @sqlString = CONCAT(@sqlString, "        SUM(IF(spa.ACTUAL AND spa.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND spa.FORECASTED_CONSUMPTION_QTY IS NOT NULL, ABS(spa.ACTUAL_CONSUMPTION_QTY-spa.FORECASTED_CONSUMPTION_QTY), null)) `DIFF_CONSUMPTION_TOTAL`, ");
+    SET @sqlString = CONCAT(@sqlString, "        SUM(IF(spa.ACTUAL AND spa.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND spa.FORECASTED_CONSUMPTION_QTY IS NOT NULL, 1, 0)) `MONTH_COUNT` ");
+    SET @sqlString = CONCAT(@sqlString, "    FROM rm_program_planning_unit ppu ");
+    SET @sqlString = CONCAT(@sqlString, "    LEFT JOIN ");
+    SET @sqlString = CONCAT(@sqlString, "        ( ");
+    SET @sqlString = CONCAT(@sqlString, "        SELECT spa.PROGRAM_ID, MAX(spa.VERSION_ID) MAX_VERSION FROM rm_supply_plan_amc spa LEFT JOIN rm_program_version pv ON spa.PROGRAM_ID=pv.PROGRAM_ID AND spa.VERSION_ID=pv.VERSION_ID ");
+    SET @sqlString = CONCAT(@sqlString, "        WHERE ");
+    SET @sqlString = CONCAT(@sqlString, "            TRUE ");
+    IF @approvedSupplyPlanOnly THEN
+        SET @sqlString = CONCAT(@sqlString, "            AND pv.VERSION_TYPE_ID=2 AND pv.VERSION_STATUS_ID=2 ");
+    END IF;
+    IF LENGTH(VAR_PROGRAM_IDS)>0 THEN
+		SET @sqlString = CONCAT(@sqlString, "            AND spa.PROGRAM_ID IN (",VAR_PROGRAM_IDS,") ");
+    END IF;
+    SET @sqlString = CONCAT(@sqlString, "        GROUP BY spa.PROGRAM_ID ");
+    SET @sqlString = CONCAT(@sqlString, "    ) f ON ppu.PROGRAM_ID=f.PROGRAM_ID ");
+    SET @sqlString = CONCAT(@sqlString, "    LEFT JOIN rm_supply_plan_amc spa ON spa.PROGRAM_ID=f.PROGRAM_ID AND spa.VERSION_ID=f.MAX_VERSION AND spa.TRANS_DATE BETWEEN SUBDATE(@startDate, INTERVAL @previousMonths MONTH) AND @startDate AND spa.PLANNING_UNIT_ID=ppu.PLANNING_UNIT_ID ");
+    SET @sqlString = CONCAT(@sqlString, "    LEFT JOIN vw_program p ON ppu.PROGRAM_ID=p.PROGRAM_ID ");
+    SET @sqlString = CONCAT(@sqlString, "    LEFT JOIN vw_planning_unit pu ON ppu.PLANNING_UNIT_ID=pu.PLANNING_UNIT_ID ");
+    SET @sqlString = CONCAT(@sqlString, "    WHERE ");
+    SET @sqlString = CONCAT(@sqlString, "        TRUE ");
+    IF LENGTH(VAR_PROGRAM_IDS)>0 THEN
+		SET @sqlString = CONCAT(@sqlString, "       AND ppu.PROGRAM_ID IN (",VAR_PROGRAM_IDS,") ");
+    END IF;
+    IF LENGTH(VAR_REALM_COUNTRY_IDS)>0 THEN
+		SET @sqlString = CONCAT(@sqlString, "       AND p.REALM_COUNTRY_ID IN (",VAR_REALM_COUNTRY_IDS,") ");
+    END IF;
+    IF LENGTH(VAR_PLANNING_UNIT_IDS)>0 THEN
+		SET @sqlString = CONCAT(@sqlString, "       AND ppu.PLANNING_UNIT_ID IN (",VAR_PLANNING_UNIT_IDS,") ");
+    END IF;
+    SET @sqlString = CONCAT(@sqlString, @aclSqlString);
+    
+    SET @sqlString = CONCAT(@sqlString, "    GROUP BY ppu.PROGRAM_ID, ppu.PLANNING_UNIT_ID ");
+    SET @sqlString = CONCAT(@sqlString, ") c2 ON spa.PROGRAM_ID=c2.PROGRAM_ID AND spa.TRANS_DATE=c2.TRANS_DATE AND spa.PLANNING_UNIT_ID=c2.PLANNING_UNIT_ID ");
+    SET @sqlString = CONCAT(@sqlString, "WHERE ");
+    SET @sqlString = CONCAT(@sqlString, "    TRUE ");
+    IF LENGTH(VAR_PROGRAM_IDS)>0 THEN
+		SET @sqlString = CONCAT(@sqlString, "       AND ppu.PROGRAM_ID IN (",VAR_PROGRAM_IDS,") ");
+    END IF;
+    IF LENGTH(VAR_REALM_COUNTRY_IDS)>0 THEN
+		SET @sqlString = CONCAT(@sqlString, "       AND p.REALM_COUNTRY_ID IN (",VAR_REALM_COUNTRY_IDS,") ");
+    END IF;
+    IF LENGTH(VAR_PLANNING_UNIT_IDS)>0 THEN
+		SET @sqlString = CONCAT(@sqlString, "       AND ppu.PLANNING_UNIT_ID IN (",VAR_PLANNING_UNIT_IDS,") ");
+    END IF;
+    SET @sqlString = CONCAT(@sqlString, @aclSqlString);
+
+    PREPARE S1 FROM @sqlString;
+    EXECUTE S1;
+END
