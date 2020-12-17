74dbd6d4864a550e3e3d432d1eb0267990abe25f
Ekta <ekta.n@altius.cc>
2020-12-17 06:05:24 +0000

    Removed the dbRestore

--- dbRestore.sh
+++
@@ -1,5 +0,0 @@
-7z x src/main/resources/fasp-db.zip
-sed -i 's/localhost/%/g' fasp-db.sql
-sed -i 's/root/faspUser/g' fasp-db.sql
-mysql -u faspUser -p -h qatuat.ckjjt1wn5lh7.us-east-1.rds.amazonaws.com fasp < fasp-db.sql
-rm fasp-db.sql
