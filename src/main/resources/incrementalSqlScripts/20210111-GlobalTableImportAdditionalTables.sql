515166e591329f222ffceb666a095606dbf1955f
Akil Mahimwala <akil.m@altius.cc>
2021-01-14 11:01:32 +0530

    Updated the script file for GlobalTables

--- src/main/resources/incrementalSqlScripts/20210111-GlobalTableImportAdditionalTables.sql
+++ src/main/resources/incrementalSqlScripts/20210111-GlobalTableImportAdditionalTables.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `fasp`.`tr_data_source` 
+ALTER TABLE `tr_data_source` 
 ADD COLUMN `PROGRAM_ID` VARCHAR(45) NOT NULL AFTER `DATA_SOURCE_ID`,
 DROP PRIMARY KEY,
 ADD PRIMARY KEY (`DataSourceID`, `PROGRAM_ID`);
@@ -18,15 +18,18 @@ CREATE TABLE `tr_funding_source` (
   PRIMARY KEY (`PipelineFunder`, `PROGRAM_ID`));
 
 drop table IF EXISTS tr_product;
-CREATE TABLE `fasp`.`tr_product` (
+CREATE TABLE `tr_product` (
   `ProductID` VARCHAR(255) NOT NULL,
   `PROGRAM_ID` INT(10) NOT NULL,
   `Min` INT NULL,
   `Max` INT NULL,
   `AMC_Months_in_Past` INT NULL,
   `AMC_Months_in_Future` INT NULL,
-  `LocalProcurement_leadTime` INT NULL,
+  `LocalProcurement_leadTime` DECIMAL(10,2) NULL,
   `ShelfLife` INT NULL,
   `DefaultPrice` DOUBLE(16,6) NULL,
   PRIMARY KEY (`ProductID`, `PROGRAM_ID`));
 
+UPDATE tr_shipment_status ss set ss.SHIPMENT_STATUS_ID=3 where ss.ShipStatusCode='Ordered';
+
+ALTER TABLE `tr_planning_unit` ADD COLUMN `UOM` DECIMAL(14,4) NULL AFTER `PSM_SKU_CODE`;
