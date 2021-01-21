aa36f67e51ff0122a9c35329e4c28fc80c821083
dolly-chhabriya-altius <dolly.c@altius.cc>
2021-01-21 23:44:56 +0530

    Query for planning unit's unit update

---
+++ src/main/resources/incrementalSqlScripts/20210119-QAT-401.sql
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  altius
+ * Created: 21-Jan-2021
+ */
+
+update rm_planning_unit p set p.UNIT_ID=7,p.LAST_MODIFIED_DATE=now() where p.PLANNING_UNIT_ID=6354;
