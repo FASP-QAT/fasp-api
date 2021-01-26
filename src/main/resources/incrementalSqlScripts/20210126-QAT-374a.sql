9447a4694085a7932d6a66bbbc5978b769a3bbdf
anchal-chouvhan-altius <anchal.c@altius.cc>
2021-01-26 10:26:42 +0530

    Split sql script

--- src/main/resources/incrementalSqlScripts/20210126-QAT-374a.sql
+++ src/main/resources/incrementalSqlScripts/20210126-QAT-374a.sql
@@ -73,17 +73,3 @@ END$$
 
 DELIMITER ;
 
-
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- Label script
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.common.million','1');
-SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
-
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Million');-- en
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Million');-- fr
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Millón');-- sp
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Milhão');-- pr
-
-
