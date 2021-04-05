96f826a80cd816f37a642c2342a277a80fc31664
palash-nikam-altius <palash.n@altius.cc>
2021-04-05 12:42:57 +0530

    commit change in desc

--- src/main/resources/incrementalSqlScripts/20210317-QAT-360.sql
+++ src/main/resources/incrementalSqlScripts/20210317-QAT-360.sql
@@ -1,7 +1,7 @@
 set @actionLabel1=(select ap.ACTION_LABEL_ID from ap_problem ap where ap.PROBLEM_ID=3);
 set @actionLabel2=(select ap.ACTION_LABEL_ID from ap_problem ap where ap.PROBLEM_ID=4);
-UPDATE ap_label SET LABEL_EN='Please check to make sure shipment <%SHIPMENT_ID%> (<%PROCUREMNET_AGENT%> | <%RO_NO%> | <%SHIPMENT_QTY%> | <%SHIPMENT_DATE%>) was received, and update either the receive date or the shipment status.' WHERE `LABEL_ID`=@actionLabel1;
-UPDATE ap_label SET LABEL_EN='Based on lead times,shipment <%SHIPMENT_ID%> (<%PROCUREMNET_AGENT%> | <%RO_NO%> | <%SHIPMENT_QTY%> | <%SHIPMENT_DATE%>) should have been submitted by <%SUBMITTED_DATE%>. Please double-check to make sure  the status or receive date is accurate.' WHERE `LABEL_ID`=@actionLabel2;
+UPDATE ap_label SET LABEL_EN='Please check if shipment <%SHIPMENT_ID%> (<%PROCUREMNET_AGENT%> | <%RO_NO%> | <%SHIPMENT_QTY%> | <%SHIPMENT_DATE%>) was received, and update the receive date and/or the shipment status.' WHERE `LABEL_ID`=@actionLabel1;
+UPDATE ap_label SET LABEL_EN='Based on lead times, shipment <%SHIPMENT_ID%> (<%PROCUREMNET_AGENT%> | <%RO_NO%> | <%SHIPMENT_QTY%> | <%SHIPMENT_DATE%>) should have been submitted by <%SUBMITTED_DATE%>. Please make sure the status and receive date are accurate.' WHERE `LABEL_ID`=@actionLabel2;
 update rm_realm_problem rrp set rrp.LAST_MODIFIED_DATE=now();
 
 INSERT INTO `ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.qpl.units','1');
