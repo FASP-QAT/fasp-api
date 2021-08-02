2b097c2442e2b0ae8ef76c2a7b530d004ddbe0a8
palash-nikam-altius <palash.n@altius.cc>
2021-08-02 19:23:46 +0530

    commit by palash

--- src/main/resources/incrementalSqlScripts/20210802-QAT-895.sql
+++ src/main/resources/incrementalSqlScripts/20210802-QAT-895.sql
@@ -1,5 +1,5 @@
 delete prt.* from rm_problem_report_trans prt where prt.PROBLEM_REPORT_ID in ( select pr.PROBLEM_REPORT_ID from rm_problem_report pr where pr.PROGRAM_ID=2034 and pr.VERSION_ID=17);
 delete pr.* from rm_problem_report pr where pr.PROGRAM_ID=2034 and pr.VERSION_ID=17;
 
-delete from rm_problem_report_trans  where rm_problem_report_trans.PROBLEM_REPORT_ID in (4356,800,802,804,852);
-delete from rm_problem_report  where rm_problem_report.PROBLEM_REPORT_ID in (4356,800,802,804,852);
+delete from rm_problem_report_trans  where rm_problem_report_trans.PROBLEM_REPORT_ID in (4356,800,802,804,852,4354,4352,4355,4353);
+delete from rm_problem_report  where rm_problem_report.PROBLEM_REPORT_ID in (4356,800,802,804,852,4354,4352,4355,4353);
