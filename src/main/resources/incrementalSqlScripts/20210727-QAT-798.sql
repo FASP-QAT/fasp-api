eac5dc6934a830c35718826b921cf5a5fdb879a9
shubham deulkar <shubham.d@altius.cc>
2021-08-09 08:21:48 +0530

    org&country field made 100 char

--- src/main/resources/incrementalSqlScripts/20210727-QAT-798.sql
+++ src/main/resources/incrementalSqlScripts/20210727-QAT-798.sql
@@ -3,4 +3,4 @@ UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Organization & Count
 UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Organización y país' WHERE `STATIC_LABEL_LANGUAGE_ID`='1478';
 UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Organisation & Pays' WHERE `STATIC_LABEL_LANGUAGE_ID`='1479';
 UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Organização e País' WHERE `STATIC_LABEL_LANGUAGE_ID`='1480';
-ALTER TABLE `fasp`.`us_user` CHANGE COLUMN `PHONE` `ORG_AND_COUNTRY` VARCHAR(15) CHARACTER SET 'utf8' NULL DEFAULT NULL ;
+ALTER TABLE `fasp`.`us_user` CHANGE COLUMN `PHONE` `ORG_AND_COUNTRY` VARCHAR(100) CHARACTER SET 'utf8' NULL DEFAULT NULL ;
