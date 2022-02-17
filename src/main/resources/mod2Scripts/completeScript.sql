52bdf85f04074c056b1c0d1c11a79d4f59a39aef
dolly chhabriya <dolly.c@altius.cc>
2022-02-17 10:10:10 +0530

    Labels queries updated

--- src/main/resources/mod2Scripts/completeScript.sql
+++ src/main/resources/mod2Scripts/completeScript.sql
@@ -6407,3 +6407,12 @@ VIEW `vw_forecast_tree_node` AS
     FROM
         (`rm_forecast_tree_node` `tn`
         LEFT JOIN `ap_label` `l` ON ((`tn`.`LABEL_ID` = `l`.`LABEL_ID`)));
+
+
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.commitTree.showValidation','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Show validation');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Afficher la validation');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Mostrar validación');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Mostrar validação');-- pr
