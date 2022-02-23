81afed7e624d07637e2a4ba3fbff62cc664ea6a5
Anchal Chauhan <anchal.c@altius.cc>
2022-02-23 23:10:08 +0530

    Committed by anchal

---
+++ 20220223-QAT-1636.sql
@@ -0,0 +1,4 @@
+DELETE FROM `fasp`.`rm_manual_tagging` WHERE `MANUAL_TAGGING_ID`='2298'; 
+DELETE FROM `fasp`.`rm_manual_tagging` WHERE `MANUAL_TAGGING_ID`='2310'; 
+DELETE FROM `fasp`.`rm_manual_tagging` WHERE `MANUAL_TAGGING_ID`='2314'; 
+UPDATE `fasp`.`rm_shipment_trans` SET `ACTIVE`='1' WHERE `SHIPMENT_TRANS_ID`='154264'; 
