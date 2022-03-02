bb74b8dd0870ee6c41d49b6a3fef3cc1f951cbb9
Akil Mahimwala <akil.m@altius.cc>
2022-03-02 12:30:35 +0530

    Added PU_Per_visit for Tree Tempalte as well

--- src/main/resources/mod2Scripts/completeScript.sql
+++ src/main/resources/mod2Scripts/completeScript.sql
@@ -6611,4 +6611,6 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Nenhuma nota de nós d
 
 ALTER TABLE `fasp`.`rm_forecast_tree_node_data_mom` ADD COLUMN `CALCULATED_MMD_VALUE` DECIMAL(16,2) UNSIGNED NOT NULL AFTER `CALCULATED_VALUE`;
 ALTER TABLE `fasp`.`rm_forecast_tree_node_data_pu` ADD COLUMN `PU_PER_VISIT` INT UNSIGNED NOT NULL AFTER `REFILL_MONTHS`;
-
+ALTER TABLE `fasp`.`rm_tree_template_node_data_pu` ADD COLUMN `PU_PER_VISIT` INT UNSIGNED NOT NULL AFTER `REFILL_MONTHS`;
+UPDATE rm_tree_template_node_data_pu SET PU_PER_VISIT = 1;
+UPDATE rm_forecast_tree_node_data_pu SET PU_PER_VISIT = 1;
