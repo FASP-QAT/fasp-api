6d44ea7f633fcb9ae7f6dd6709eddfb935645527
Akil Mahimwala <akil.m@altius.cc>
2022-03-03 11:14:39 +0530

    Split the script files

---
+++ src/main/resources/mod2Scripts/Sprint3.sql
@@ -0,0 +1,6 @@
+
+ALTER TABLE `fasp`.`rm_forecast_tree_node_data_mom` ADD COLUMN `CALCULATED_MMD_VALUE` DECIMAL(16,2) UNSIGNED NOT NULL AFTER `CALCULATED_VALUE`;
+ALTER TABLE `fasp`.`rm_forecast_tree_node_data_pu` ADD COLUMN `PU_PER_VISIT` INT UNSIGNED NOT NULL AFTER `REFILL_MONTHS`;
+ALTER TABLE `fasp`.`rm_tree_template_node_data_pu` ADD COLUMN `PU_PER_VISIT` INT UNSIGNED NOT NULL AFTER `REFILL_MONTHS`;
+UPDATE rm_tree_template_node_data_pu SET PU_PER_VISIT = 1;
+UPDATE rm_forecast_tree_node_data_pu SET PU_PER_VISIT = 1;
