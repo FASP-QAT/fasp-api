9ef10601b1f72bec158be9ca25718455f282285d
Sonal Kothari <sonal.k@altius.cc>
2022-04-07 17:52:47 +0530

    Sonal - label query added

--- src/main/resources/mod2Scripts/Sprint8.sql
+++ src/main/resources/mod2Scripts/Sprint8.sql
@@ -75,3 +75,20 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'change');-- en
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'changement');-- fr
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'cambio');-- sp
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'mudança');-- pr
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.dataentry.otherUnitName','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Enter Other unit name');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Entrez le nom de l`autre unité');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Ingrese el nombre de otra unidad');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Digite o nome de outra unidade');-- pr
+
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.dataentry.otherUnitMultiplier','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Enter Other unit multiplier');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Entrez Autre multiplicateur d`unité');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Ingrese el multiplicador de otra unidad');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Insira outro multiplicador de unidade');-- pr
