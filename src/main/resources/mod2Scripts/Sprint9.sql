5e830a498985d60bc99984aa72687868420e9c3a
Anchal Chauhan <anchal.c@altius.cc>
2022-04-12 12:34:47 +0530

    Committed by anchal

--- src/main/resources/mod2Scripts/Sprint9.sql
+++ src/main/resources/mod2Scripts/Sprint9.sql
@@ -44,3 +44,23 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,' in ');-- en
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,' dans ');-- fr
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,' en ');-- sp
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,' dentro ');-- pr
+
+UPDATE ap_static_label l 
+LEFT JOIN ap_static_label_languages ll ON l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+SET ll.LABEL_TEXT='Transfer'
+WHERE l.LABEL_CODE='static.tree.transferToNode' AND ll.LANGUAGE_ID=1;
+
+UPDATE ap_static_label l 
+LEFT JOIN ap_static_label_languages ll ON l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+SET ll.LABEL_TEXT='Transférer'
+WHERE l.LABEL_CODE='static.tree.transferToNode' AND ll.LANGUAGE_ID=2;
+
+UPDATE ap_static_label l 
+LEFT JOIN ap_static_label_languages ll ON l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+SET ll.LABEL_TEXT='Transferir'
+WHERE l.LABEL_CODE='static.tree.transferToNode' AND ll.LANGUAGE_ID=3;
+
+UPDATE ap_static_label l 
+LEFT JOIN ap_static_label_languages ll ON l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+SET ll.LABEL_TEXT='Transferir'
+WHERE l.LABEL_CODE='static.tree.transferToNode' AND ll.LANGUAGE_ID=4;
