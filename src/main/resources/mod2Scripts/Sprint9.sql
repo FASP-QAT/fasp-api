b9682071c7590b3a33e7d28e3b7d048504bb9097
Sonal Kothari <sonal.k@altius.cc>
2022-04-18 11:01:32 +0530

    Sonal - script added for label

--- src/main/resources/mod2Scripts/Sprint9.sql
+++ src/main/resources/mod2Scripts/Sprint9.sql
@@ -44,3 +44,12 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,' in ');-- en
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,' dans ');-- fr
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,' en ');-- sp
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,' dentro ');-- pr
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.importIntoQATSupplyPlan.pleaseSelectForecastProgramVersion','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Please select forecast program version');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Veuillez sélectionner la version du programme de prévision');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Seleccione la versión del programa de previsión');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Selecione a versão do programa de previsão');-- pr
+
