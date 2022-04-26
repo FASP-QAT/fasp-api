6cbcbeb2fc73f4ea909c45f9e7a2ec6c6eea80e5
Anchal Chauhan <anchal.c@altius.cc>
2022-04-26 17:46:37 +0530

    Committed by anchal

--- src/main/resources/mod2Scripts/Sprint11.sql
+++ src/main/resources/mod2Scripts/Sprint11.sql
@@ -30,7 +30,7 @@ UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Esses nomes de uso a
 
 UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Link the changes in this node to the changes in another node in the same level. If a row is greyed out, edit on source (or \'from\') node.' WHERE `STATIC_LABEL_LANGUAGE_ID`='200322'; 
 
-UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Liez les modifications de ce nœud aux modifications d\'un autre nœud du même niveau. Si une ligne est grisée, modifiez-la sur le nœud source (ou « de »).' WHERE `STATIC_LABEL_LANGUAGE_ID`='200323'; 
+UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Liez les modifications de ce nœud aux modifications d\un autre nœud du même niveau. Si une ligne est grisée, modifiez-la sur le nœud source (ou « de »).' WHERE `STATIC_LABEL_LANGUAGE_ID`='200323'; 
 
 UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='Vincule los cambios en este nodo a los cambios en otro nodo en el mismo nivel. Si una fila está atenuada, edite en el nodo fuente (o \'desde\').' WHERE `STATIC_LABEL_LANGUAGE_ID`='200324'; 
 
@@ -40,6 +40,28 @@ INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NUL
 SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
 
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Conversion Factor (Forecast Planning Unit to Supply Plan)');-- en
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Facteur de conversion (prévision en plan d`approvisionnement)');-- fr
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Fator de conversão (previsão para plano de fornecimento)');-- pr
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Factor de conversión (pronóstico a plan de suministro)');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Facteur de conversion (prévision en plan dapprovisionnement)');
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Fator de conversão (previsão para plano de fornecimento)');
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Factor de conversión (pronóstico a plan de suministro)');
+
+
+
+update ap_static_label l 
+left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+set ll.LABEL_TEXT='NOTE:  The minimum values needed to get correct graphs and reports for the various features are below:\n* TES, Holt-Winters:  Needs at least 24 months of actual consumption data\n* ARIMA:  Needs at least 14 months of actual consumption data\n* Moving Average, Semi-Averages, and Linear Regression:  Needs at least 3 months of actual consumption data'
+where l.LABEL_CODE='static.tree.minDataRequiredToExtrapolate' and ll.LANGUAGE_ID=1;
+
+update ap_static_label l 
+left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+set ll.LABEL_TEXT='REMARQUE : Les valeurs minimales nécessaires pour obtenir des graphiques et des rapports corrects pour les différentes fonctionnalités sont les suivantes :\n* TES, Holt-Winters : nécessite au moins 24 mois de données de consommation réelle\n* ARIMA : nécessite au moins 14 mois de données de consommation réelle\n* Moyenne mobile, semi-moyennes et régression linéaire : nécessite au moins 3 mois de données de consommation réelle'
+where l.LABEL_CODE='static.tree.minDataRequiredToExtrapolate' and ll.LANGUAGE_ID=2;
+
+update ap_static_label l 
+left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+set ll.LABEL_TEXT='NOTA: Los valores mínimos necesarios para obtener gráficos e informes correctos para las diversas funciones se encuentran a continuación:\n* TES, Holt-Winters: necesita al menos 24 meses de datos de consumo real\n* ARIMA: necesita al menos 14 meses de datos de consumo real\n* Promedio móvil, semipromedios y regresión lineal: necesita al menos 3 meses de datos de consumo real'
+where l.LABEL_CODE='static.tree.minDataRequiredToExtrapolate' and ll.LANGUAGE_ID=3;
+
+update ap_static_label l 
+left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+set ll.LABEL_TEXT='OBSERVAÇÃO: os valores mínimos necessários para obter gráficos e relatórios corretos para os vários recursos estão abaixo:\n* TES, Holt-Winters: precisa de pelo menos 24 meses de dados de consumo reais\n* ARIMA: precisa de pelo menos 14 meses de dados de consumo reais\n* Média Móvel, Semi-Médias e Regressão Linear: Precisa de pelo menos 3 meses de dados de consumo real'
+where l.LABEL_CODE='static.tree.minDataRequiredToExtrapolate' and ll.LANGUAGE_ID=4;
