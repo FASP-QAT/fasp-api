28722d27b3792709e8ff174f5801c4ac412ffbd1
dolly chhabriya <dolly.c@altius.cc>
2022-04-27 14:14:33 +0530

    Labels script

--- src/main/resources/mod2Scripts/Sprint12.sql
+++ src/main/resources/mod2Scripts/Sprint12.sql
@@ -15,3 +15,11 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Version (* indicates F
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Version (* indique Finale)');-- fr
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Versión (* indica Final)');-- sp
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Versão (* indica Final)');-- pr
+
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.common.person','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Person');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Personne');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Persona');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Pessoa');-- pr
