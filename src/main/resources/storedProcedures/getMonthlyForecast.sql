8a49a7c9512dacfa6d36e812946166f21e9e4f39
Akil Mahimwala <akil.m@altius.cc>
2022-05-07 08:55:27 +0530

    Updated the Monthly Forecast SP to include months with no Forecast

--- src/main/resources/storedProcedures/getMonthlyForecast.sql
+++ src/main/resources/storedProcedures/getMonthlyForecast.sql
@@ -29,6 +29,7 @@ BEGIN
         LEFT JOIN rm_dataset_planning_unit_selected dpus ON dpu.PROGRAM_PLANNING_UNIT_ID=dpus.PROGRAM_PLANNING_UNIT_ID
         LEFT JOIN vw_planning_unit pu ON dpu.PLANNING_UNIT_ID=pu.PLANNING_UNIT_ID
         LEFT JOIN vw_forecasting_unit fu ON pu.FORECASTING_UNIT_ID=fu.FORECASTING_UNIT_ID
+        LEFT JOIN mn ON mn.MONTH BETWEEN @startMonth AND @stopMonth 
         LEFT JOIN 
             (
             SELECT 
@@ -38,17 +39,18 @@ BEGIN
             LEFT JOIN rm_forecast_tree_node_data_pu ftndpu ON ftndpu.NODE_DATA_PU_ID=ftnd.NODE_DATA_PU_ID 
             WHERE ftn.NODE_TYPE_ID=5
         ) tree ON dpus.TREE_ID=tree.TREE_ID AND tree.SCENARIO_ID=dpus.SCENARIO_ID AND pu.PLANNING_UNIT_ID=tree.PLANNING_UNIT_ID
-        LEFT JOIN rm_forecast_tree_node_data_mom mom ON mom.NODE_DATA_ID=tree.NODE_DATA_ID AND mom.MONTH BETWEEN @startMonth AND @stopMonth 
+        LEFT JOIN rm_forecast_tree_node_data_mom mom ON mom.NODE_DATA_ID=tree.NODE_DATA_ID AND mom.MONTH=mn.MONTH
         LEFT JOIN vw_region r ON dpus.REGION_ID=r.REGION_ID
         LEFT JOIN vw_forecast_tree t ON dpus.TREE_ID=t.TREE_ID
         LEFT JOIN vw_scenario s ON dpus.SCENARIO_ID=s.SCENARIO_ID
         LEFT JOIN rm_forecast_consumption_extrapolation fce ON dpus.CONSUMPTION_EXTRAPOLATION_ID=fce.CONSUMPTION_EXTRAPOLATION_ID
         LEFT JOIN vw_extrapolation_method em on fce.EXTRAPOLATION_METHOD_ID=em.EXTRAPOLATION_METHOD_ID
-        LEFT JOIN rm_forecast_consumption_extrapolation_data fced ON fce.CONSUMPTION_EXTRAPOLATION_ID=fced.CONSUMPTION_EXTRAPOLATION_ID AND fced.MONTH BETWEEN @startMonth AND @stopMonth
+        LEFT JOIN rm_forecast_consumption_extrapolation_data fced ON fce.CONSUMPTION_EXTRAPOLATION_ID=fced.CONSUMPTION_EXTRAPOLATION_ID AND fced.MONTH=mn.MONTH
         WHERE 
             dpu.PROGRAM_ID=@programId 
             AND dpu.VERSION_ID=@versionId 
-            AND FIND_IN_SET(pu.PLANNING_UNIT_ID, @unitIdList);
+            AND FIND_IN_SET(pu.PLANNING_UNIT_ID, @unitIdList)
+	ORDER BY pu.PLANNING_UNIT_ID, r.REGION_ID, mn.MONTH;
     ELSEIF @reportView = 2 THEN
         SELECT 
             pu.PLANNING_UNIT_ID, pu.LABEL_ID `PU_LABEL_ID`, pu.LABEL_EN `PU_LABEL_EN`, pu.LABEL_FR `PU_LABEL_FR`, pu.LABEL_SP `PU_LABEL_SP`, pu.LABEL_PR `PU_LABEL_PR`, pu.MULTIPLIER,
