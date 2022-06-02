87b603ed69dd9b9eeb2f721c2c4f9dc38a1b8ffd
dolly chhabriya <dolly.c@altius.cc>
2022-06-02 11:27:25 +0530

    Commit changes

--- src/main/java/cc/altius/FASP/rest/controller/CommitRequestRestController.java
+++ src/main/java/cc/altius/FASP/rest/controller/CommitRequestRestController.java
@@ -179,9 +179,10 @@ public class CommitRequestRestController {
     }
 
     // Part 2 of the Commit Request
-    @GetMapping("/processCommitRequest")
+//    @GetMapping("/processCommitRequest")
     //sec min hour day_of_month month day_of_week
-    @Scheduled(cron = "00 */1 * * * *")
+//    @Scheduled(cron = "00 */1 * * * *")
+     @Scheduled(fixedDelay = 60000, initialDelay = 60000)//fixedDelay=1mins and initialDelay=1min
     public ResponseEntity processCommitRequest() {
         try {
             logger.info("Starting the Commit request scheduler");
