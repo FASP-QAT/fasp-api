a6659cdfa7e57a00e673a441f9b881369afeb1b7
altius <seema.s@altius.cc>
2022-08-30 13:10:32 +0530

    UPdate querried modified

--- src/main/resources/incrementalSqlScripts/QAT-2087.sql
+++ src/main/resources/incrementalSqlScripts/QAT-2087.sql
@@ -9,16 +9,16 @@
  */
 
 -- en
-UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='QAT is unable to calculate ARIMA forecast based on the selected actual consumption. Generally, ARIMA should be able to calculate with 14 months of data.' WHERE `STATIC_LABEL_LANGUAGE_ID`='266629'; 
+UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='QAT is unable to calculate ARIMA forecast based on the selected actual consumption. Generally, ARIMA should be able to calculate with 14 months of data.' WHERE `STATIC_LABEL_LANGUAGE_ID`='271821'; 
 
 -- fn
-UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='QAT nest pas en mesure de calculer la prévision ARIMA en fonction de la consommation réelle sélectionnée. Généralement, ARIMA devrait pouvoir calculer avec 14 mois de données.' WHERE `STATIC_LABEL_LANGUAGE_ID`='266630'; 
+UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='QAT nest pas en mesure de calculer la prévision ARIMA en fonction de la consommation réelle sélectionnée. Généralement, ARIMA devrait pouvoir calculer avec 14 mois de données.' WHERE `STATIC_LABEL_LANGUAGE_ID`='271822'; 
 
 -- sp
-UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='QAT no puede calcular el pronóstico ARIMA en función del consumo real seleccionado. En general, ARIMA debería poder calcular con 14 meses de datos.' WHERE `STATIC_LABEL_LANGUAGE_ID`='266631'; 
+UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='QAT no puede calcular el pronóstico ARIMA en función del consumo real seleccionado. En general, ARIMA debería poder calcular con 14 meses de datos.' WHERE `STATIC_LABEL_LANGUAGE_ID`='271823'; 
 
 -- pr
-UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='A QAT não consegue calcular a previsão ARIMA com base no consumo real selecionado. Geralmente, o ARIMA deve ser capaz de calcular com 14 meses de dados.' WHERE `STATIC_LABEL_LANGUAGE_ID`='266632'; 
+UPDATE `fasp`.`ap_static_label_languages` SET `LABEL_TEXT`='A QAT não consegue calcular a previsão ARIMA com base no consumo real selecionado. Geralmente, o ARIMA deve ser capaz de calcular com 14 meses de dados.' WHERE `STATIC_LABEL_LANGUAGE_ID`='271824'; 
 
 
 
