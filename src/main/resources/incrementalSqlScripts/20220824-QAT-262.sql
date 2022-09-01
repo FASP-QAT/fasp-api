ad2f1d9ec2558443da85eceee8bd49a006998392
dolly chhabriya <dolly.c@altius.cc>
2022-09-02 00:45:18 +0530

    Added PU and ARU in other linked check api

--- src/main/resources/incrementalSqlScripts/20220824-QAT-262.sql
+++ src/main/resources/incrementalSqlScripts/20220824-QAT-262.sql
@@ -289,3 +289,11 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'ERP Qty');-- en
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Qté ERP');-- fr
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Cantidad ERP');-- sp
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Qtde ERP');-- pr
+
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.manualTagging.conversionERPToPU','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Conversion Factor (ERP to QAT PU)');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Facteur de conversion (ERP à QAT PU)');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Factor de conversión (ERP a QAT PU)');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Fator de conversão (ERP para QAT PU)');-- pr
