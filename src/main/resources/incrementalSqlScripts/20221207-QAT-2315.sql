a0d2dce5c9b6af2e6eb671fb45fff1841fcab908
dolly chhabriya <dolly.c@altius.cc>
2022-12-09 23:57:51 +0530

    Bug in scripts fixed

--- src/main/resources/incrementalSqlScripts/20221207-QAT-2315.sql
+++ src/main/resources/incrementalSqlScripts/20221207-QAT-2315.sql
@@ -41,14 +41,6 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Lista de controle de a
 INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.dataset.version1Settings','1'); 
 SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
 
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'');-- en
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'');-- fr
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'');-- sp
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'');-- pr
-
-INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.dataset.version1Settings','1'); 
-SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
-
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Version 1 Settings');-- en
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Paramètres de la version 1');-- fr
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Configuración de la versión 1');-- sp
