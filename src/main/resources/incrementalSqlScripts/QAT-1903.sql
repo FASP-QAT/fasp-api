a2dccd703a3891f8e52e7c0534fc7eb13239d250
dolly chhabriya <dolly.c@altius.cc>
2023-01-11 12:14:45 +0530

    Added changes for view

--- src/main/resources/incrementalSqlScripts/QAT-1903.sql
+++ src/main/resources/incrementalSqlScripts/QAT-1903.sql
@@ -9,3 +9,30 @@
 
 ALTER TABLE `rm_tree_template` 
 DROP COLUMN `BRANCH`;
+
+USE `fasp`;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `faspUser`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `vw_tree_template` AS
+    SELECT 
+        `tt`.`TREE_TEMPLATE_ID` AS `TREE_TEMPLATE_ID`,
+        `tt`.`REALM_ID` AS `REALM_ID`,
+        `tt`.`LABEL_ID` AS `LABEL_ID`,
+        `tt`.`FORECAST_METHOD_ID` AS `FORECAST_METHOD_ID`,
+        `tt`.`MONTHS_IN_PAST` AS `MONTHS_IN_PAST`,
+        `tt`.`MONTHS_IN_FUTURE` AS `MONTHS_IN_FUTURE`,
+        `tt`.`CREATED_BY` AS `CREATED_BY`,
+        `tt`.`CREATED_DATE` AS `CREATED_DATE`,
+        `tt`.`LAST_MODIFIED_BY` AS `LAST_MODIFIED_BY`,
+        `tt`.`LAST_MODIFIED_DATE` AS `LAST_MODIFIED_DATE`,
+        `tt`.`ACTIVE` AS `ACTIVE`,
+        `l`.`LABEL_EN` AS `LABEL_EN`,
+        `l`.`LABEL_FR` AS `LABEL_FR`,
+        `l`.`LABEL_SP` AS `LABEL_SP`,
+        `l`.`LABEL_PR` AS `LABEL_PR`,
+        `tt`.`NOTES` AS `NOTES`
+    FROM
+        (`rm_tree_template` `tt`
+        LEFT JOIN `ap_label` `l` ON ((`tt`.`LABEL_ID` = `l`.`LABEL_ID`)));
