24354829a5e4189ae26c7b090587511e498bc7a8
dolly chhabriya <dolly.c@altius.cc>
2023-01-11 11:46:09 +0530

    Removed branch column and related changes

---
+++ src/main/resources/incrementalSqlScripts/QAT-1903.sql
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  altius
+ * Created: 11-Jan-2023
+ */
+
+ALTER TABLE `rm_tree_template` 
+DROP COLUMN `BRANCH`;
