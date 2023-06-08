b9d659984585e79ed08217940c38c9d9d7bf20e6
dolly chhabriya <dolly.c@altius.cc>
2023-06-08 08:21:31 +0530

    Added labels scripts

--- src/main/resources/incrementalSqlScripts/20230606-QAT-2867.sql
+++ src/main/resources/incrementalSqlScripts/20230606-QAT-2867.sql
@@ -37,3 +37,46 @@ VIEW `vw_realm` AS
     FROM
         (`rm_realm` `r`
         LEFT JOIN `ap_label` `rl` ON ((`r`.`LABEL_ID` = `rl`.`LABEL_ID`)));
+
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.realm.restrictionActualConsumption','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Data entry restriction for Actual Consumption');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Restriction de saisie de données pour la consommation réelle');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Restricción de entrada de datos para consumo real');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Restrição de entrada de dados para Consumo Real');-- pr
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.realm.restrictionForecastConsumption','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Data entry restriction for Forecast Consumption');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Restriction de saisie de données pour la consommation prévisionnelle');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Restricción de entrada de datos para consumo de pronóstico');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Restrição de entrada de dados para previsão de consumo');-- pr
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.realm.restrictionInventory','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Data entry restriction for Inventory');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Restriction de saisie de données pour l`inventaire');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Restricción de entrada de datos para Inventario');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Restrição de entrada de dados para Inventário');-- pr
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.validated.restrictionActualConsumption','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Please enter data entry restriction for Actual Consumption');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Veuillez entrer une restriction de saisie de données pour la consommation réelle');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Ingrese la restricción de entrada de datos para el consumo real');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Insira a restrição de entrada de dados para consumo real');-- pr
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.validated.restrictionForecastConsumption','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Please enter data entry restriction for Forecast Consumption');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Veuillez saisir une restriction de saisie de données pour la consommation prévue');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Ingrese la restricción de entrada de datos para el consumo de pronóstico');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Insira a restrição de entrada de dados para a previsão de consumo');-- pr
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.validated.restrictionInventory','1'); 
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Please enter data entry restriction for Inventory');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Veuillez entrer la restriction de saisie de données pour l`inventaire');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Ingrese la restricción de entrada de datos para el inventario');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Insira a restrição de entrada de dados para Inventário');-- pr
