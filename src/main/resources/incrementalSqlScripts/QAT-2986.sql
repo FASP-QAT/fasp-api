0bb280bc96a8318d352d2135269e6bf6d7efe438
Rohit <rohit.v@altius.cc>
2023-06-09 14:22:26 +0530

    Updated static labels

--- src/main/resources/incrementalSqlScripts/QAT-2986.sql
+++ src/main/resources/incrementalSqlScripts/QAT-2986.sql
@@ -14,3 +14,23 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Only letters, spaces, 
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Seuls les lettres, des espaces, chiffres et caractères spéciaux sont autorisés. Veuillez vérifier les caractères non valides.');-- fr
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Solo se permiten letras, espacios, números y caracteres especiales. Compruebe si hay caracteres no válidos.');-- sp
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Somente letras, espaços, números e caracteres especiais são permitidos. Verifique se há caracteres inválidos.');-- pr
+
+update ap_static_label l 
+left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+set ll.LABEL_TEXT='Only letters, spaces, numbers and the characters ` ~ ! @ # $ % ^ & ( ) _ { } : \" < > ? , . ; \' [ ] / \\ * - + = | are allowed. Please check for invalid characters.'
+where l.LABEL_CODE='static.commit.consumptionnotesvalid' and ll.LANGUAGE_ID=1;
+
+update ap_static_label l 
+left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+set ll.LABEL_TEXT='Seuls les lettres, les espaces, les chiffres et les caractères ` ~ ! @ # $ % ^ & ( ) _ { } : \" < > ? , . ; \' [ ] / \\ * - + = | sont autorisés. Veuillez vérifier les caractères invalides.'
+where l.LABEL_CODE='static.commit.consumptionnotesvalid' and ll.LANGUAGE_ID=2;
+
+update ap_static_label l 
+left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+set ll.LABEL_TEXT='¡Solo letras, espacios, números y los caracteres ` ~ ! @ # $ % ^ & ( ) _ { } : \" < > ? , . ; \' [ ] / \\ * - + = | están permitidos. Compruebe si hay caracteres no válidos.'
+where l.LABEL_CODE='static.commit.consumptionnotesvalid' and ll.LANGUAGE_ID=3;
+
+update ap_static_label l 
+left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
+set ll.LABEL_TEXT='Somente letras, espaços, números e os caracteres ` ~ ! @ # $ % ^ & ( ) _ { } : \" < > ? , . ; \' [ ] / \\ * - + = | são permitidos. Verifique se há caracteres inválidos.'
+where l.LABEL_CODE='static.commit.consumptionnotesvalid' and ll.LANGUAGE_ID=4;
