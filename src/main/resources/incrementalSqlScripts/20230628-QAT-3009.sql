189539e3ef0db4503aadc0adebd7c94e84d11539
dolly chhabriya <dolly.c@altius.cc>
2023-07-03 17:58:32 +0530

    Changed label script

--- src/main/resources/incrementalSqlScripts/20230628-QAT-3009.sql
+++ src/main/resources/incrementalSqlScripts/20230628-QAT-3009.sql
@@ -18,7 +18,7 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Falta o preço - adici
 INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.forecastSummary.priceIsMissing','1'); 
 SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
 
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Price is missing');-- en
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Le prix est manquant');-- fr
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Falta el precio');-- sp
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'preço está faltando');-- pr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Price is missing - please add in Update Planning Unit screen (click to open new tab)');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Le prix est manquant - veuillez l`ajouter dans l`écran Mettre à jour l`unité de planification (cliquez pour ouvrir un nouvel onglet)');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Falta el precio: agréguelo en la pantalla Actualizar unidad de planificación (haga clic para abrir una nueva pestaña)');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Falta o preço - adicione na tela Atualizar unidade de planejamento (clique para abrir uma nova guia)');-- pr
