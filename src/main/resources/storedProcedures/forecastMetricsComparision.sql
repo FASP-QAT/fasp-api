b9e723b35b462ac8685e79075d1dde49f48bbcfb
Akil Mahimwala <akil.m@altius.cc>
2023-08-06 10:01:58 +0530

    Updated the querry

--- src/main/resources/storedProcedures/forecastMetricsComparision.sql
+++ src/main/resources/storedProcedures/forecastMetricsComparision.sql
@@ -1,13 +1,13 @@
 CREATE DEFINER=`faspUser`@`%` PROCEDURE `forecastMetricsComparision`( 
-    VAR_USER_ID INT(10), 
-    VAR_REALM_ID INT(10), 
-    VAR_START_DATE DATE, 
-    VAR_REALM_COUNTRY_IDS TEXT, 
-    VAR_PROGRAM_IDS TEXT, 
-    VAR_TRACER_CATEGORY_IDS TEXT, 
-    VAR_PLANNING_UNIT_IDS TEXT,  
-    VAR_PREVIOUS_MONTHS INT(10), 
-    VAR_APPROVED_SUPPLY_PLAN_ONLY TINYINT(1))
+	VAR_USER_ID INT(10), 
+	VAR_REALM_ID INT(10), 
+	VAR_START_DATE DATE, 
+	VAR_REALM_COUNTRY_IDS TEXT, 
+	VAR_PROGRAM_IDS TEXT, 
+	VAR_TRACER_CATEGORY_IDS TEXT, 
+	VAR_PLANNING_UNIT_IDS TEXT,  
+	VAR_PREVIOUS_MONTHS INT(10), 
+	VAR_APPROVED_SUPPLY_PLAN_ONLY TINYINT(1))
 BEGIN
 
     -- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -50,91 +50,85 @@ BEGIN
     END LOOP;
     CLOSE cursor_acl;
     SET @aclSqlString = CONCAT(@aclSqlString, ") ");
-    
-    SET @tableName = CONCAT(
-			"tmp_forecastingMetric_"
-            ,substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand()*36+1, 1)
-            ,substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand()*36+1, 1)
-            ,substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand()*36+1, 1)
-            ,substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', rand()*36+1, 1)
-		);
-    SET @realmId = VAR_REALM_ID;
-    SET @startDate = VAR_START_DATE;
-    SET @previousMonths = VAR_PREVIOUS_MONTHS;
-    SET @approvedSupplyPlanOnly = VAR_APPROVED_SUPPLY_PLAN_ONLY;
-    SET @realmCountryIds = VAR_REALM_COUNTRY_IDS;
-    SET @programIds = VAR_PROGRAM_IDS;
-    SET @tracerCategoryIds = VAR_TRACER_CATEGORY_IDS;
-    SET @planningUnitIds = VAR_PLANNING_UNIT_IDS;
+
+    SET @varRealmId = VAR_REALM_ID;
+    SET @varStopDate = VAR_START_DATE;
+    SET @varStartDate = SUBDATE(VAR_START_DATE, INTERVAL VAR_PREVIOUS_MONTHS-1 MONTH);
+    SET @varApprovedSupplyPlanOnly = VAR_APPROVED_SUPPLY_PLAN_ONLY;
+    SET @varRealmCountryIds = VAR_REALM_COUNTRY_IDS;
+    SET @varProgramIds = VAR_PROGRAM_IDS;
+    SET @varTracerCategoryIds = VAR_TRACER_CATEGORY_IDS;
+    SET @varPlanningUnitIds = VAR_PLANNING_UNIT_IDS;
     
     
-    SET @sqlString = "";
-    
-    SET @sqlString = CONCAT(@sqlString, "CREATE TABLE ",@tableName," (");
-	SET @sqlString = CONCAT(@sqlString, "  `PROGRAM_ID` int unsigned NOT NULL,");
-	SET @sqlString = CONCAT(@sqlString, "  `VERSION_ID` int unsigned NOT NULL,");
-	SET @sqlString = CONCAT(@sqlString, "  `PLANNING_UNIT_ID` int unsigned NOT NULL,");
-	SET @sqlString = CONCAT(@sqlString, "  `TRANS_DATE` date NOT NULL,");
-	SET @sqlString = CONCAT(@sqlString, "  `ACTUAL` tinyint(1) NOT NULL,");
-	SET @sqlString = CONCAT(@sqlString, "  `ACTUAL_CONSUMPTION_QTY` int DEFAULT NULL,");
-	SET @sqlString = CONCAT(@sqlString, "  `FORECASTED_CONSUMPTION_QTY` int DEFAULT NULL,");
-	SET @sqlString = CONCAT(@sqlString, "  PRIMARY KEY (`PROGRAM_ID`,`VERSION_ID`,`PLANNING_UNIT_ID`,`TRANS_DATE`)");
-	SET @sqlString = CONCAT(@sqlString, ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;");
-	PREPARE S1 FROM @sqlString;
-    EXECUTE S1;
-    
-    SET @sqlString = "";
-    SET @sqlString = CONCAT(@sqlString, "INSERT INTO ",@tableName," ");
-	SET @sqlString = CONCAT(@sqlString, "	SELECT p1.PROGRAM_ID, p1.MAX_VERSION, pu.PLANNING_UNIT_ID, spa.TRANS_DATE, spa.ACTUAL, spa.ACTUAL_CONSUMPTION_QTY, spa.FORECASTED_CONSUMPTION_QTY");
-	SET @sqlString = CONCAT(@sqlString, "	FROM (");
-	SET @sqlString = CONCAT(@sqlString, "		SELECT ");
-	SET @sqlString = CONCAT(@sqlString, "			pv.PROGRAM_ID, MAX(pv.VERSION_ID) MAX_VERSION ");
-	SET @sqlString = CONCAT(@sqlString, "		FROM rm_program_version pv ");
-	SET @sqlString = CONCAT(@sqlString, "		LEFT JOIN rm_program p ON pv.PROGRAM_ID=p.PROGRAM_ID");
-	SET @sqlString = CONCAT(@sqlString, "		WHERE ");
-	SET @sqlString = CONCAT(@sqlString, "			TRUE ");
-	SET @sqlString = CONCAT(@sqlString, "			AND (@approvedSupplyPlanOnly!=1 OR (@approvedSupplyPlanOnly=1 AND pv.VERSION_TYPE_ID=2 AND pv.VERSION_STATUS_ID=2))");
-	SET @sqlString = CONCAT(@sqlString, "			AND p.ACTIVE");
-	SET @sqlString = CONCAT(@sqlString, "			AND (LENGTH(@programIds)=0 OR FIND_IN_SET(pv.PROGRAM_ID, @programIds))");
-	SET @sqlString = CONCAT(@sqlString, "			AND (LENGTH(@realmCountryIds)=0 OR FIND_IN_SET(p.REALM_COUNTRY_ID, @realmCountryIds))");
-	SET @sqlString = CONCAT(@sqlString, "		GROUP BY pv.PROGRAM_ID");
-	SET @sqlString = CONCAT(@sqlString, "		) p1");
-	SET @sqlString = CONCAT(@sqlString, "	LEFT JOIN rm_program_planning_unit ppu ON ppu.PROGRAM_ID=p1.PROGRAM_ID");
-	SET @sqlString = CONCAT(@sqlString, "	LEFT JOIN rm_planning_unit pu ON ppu.PLANNING_UNIT_ID=pu.PLANNING_UNIT_ID");
-	SET @sqlString = CONCAT(@sqlString, "	LEFT JOIN rm_forecasting_unit fu ON fu.FORECASTING_UNIT_ID=pu.FORECASTING_UNIT_ID");
-	SET @sqlString = CONCAT(@sqlString, "	LEFT JOIN rm_supply_plan_amc spa ON p1.PROGRAM_ID=spa.PROGRAM_ID AND p1.MAX_VERSION=spa.VERSION_ID AND spa.PLANNING_UNIT_ID=pu.PLANNING_UNIT_ID");
-	SET @sqlString = CONCAT(@sqlString, "	WHERE ");
-	SET @sqlString = CONCAT(@sqlString, "		TRUE");
-	SET @sqlString = CONCAT(@sqlString, "		AND pu.PLANNING_UNIT_ID IS NOT NULL");
-	SET @sqlString = CONCAT(@sqlString, "		AND (LENGTH(@planningUnitIds)=0 OR FIND_IN_SET(ppu.PLANNING_UNIT_ID, @planningUnitIds))");
-	SET @sqlString = CONCAT(@sqlString, "		AND (LENGTH(@tracerCategoryIds)=0 OR FIND_IN_SET(fu.TRACER_CATEGORY_ID, @tracerCategoryIds))");
-	SET @sqlString = CONCAT(@sqlString, "		AND spa.TRANS_DATE BETWEEN SUBDATE(@startDate, INTERVAL @previousMonths MONTH) AND @startDate;");
-    PREPARE S2 FROM @sqlString;
-    EXECUTE S2;
-    
-	
+    DROP TABLE IF EXISTS tmp_forecastMetrics1;
+    CREATE TEMPORARY TABLE tmp_forecastMetrics1 (
+        `PROGRAM_ID` int unsigned NOT NULL,
+        `VERSION_ID` int unsigned NOT NULL,
+        `PLANNING_UNIT_ID` int unsigned NOT NULL,
+        `TRANS_DATE` date NOT NULL,
+        `ACTUAL` tinyint(1) NOT NULL,
+        `ACTUAL_CONSUMPTION_QTY` int DEFAULT NULL,
+        `FORECASTED_CONSUMPTION_QTY` int DEFAULT NULL,
+        PRIMARY KEY (`PROGRAM_ID`,`VERSION_ID`,`PLANNING_UNIT_ID`,`TRANS_DATE`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+    DROP TABLE IF EXISTS tmp_forecastMetrics2;
+    CREATE TEMPORARY TABLE tmp_forecastMetrics2 (
+        `PROGRAM_ID` int unsigned NOT NULL,
+        `VERSION_ID` int unsigned NOT NULL,
+        `PLANNING_UNIT_ID` int unsigned NOT NULL,
+        `TRANS_DATE` date NOT NULL,
+        `ACTUAL` tinyint(1) NOT NULL,
+        `ACTUAL_CONSUMPTION_QTY` int DEFAULT NULL,
+        `FORECASTED_CONSUMPTION_QTY` int DEFAULT NULL,
+        PRIMARY KEY (`PROGRAM_ID`,`VERSION_ID`,`PLANNING_UNIT_ID`,`TRANS_DATE`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+    INSERT INTO tmp_forecastMetrics1
+	SELECT p1.PROGRAM_ID, p1.MAX_VERSION, spa.PLANNING_UNIT_ID, spa.TRANS_DATE, spa.ACTUAL, spa.ACTUAL_CONSUMPTION_QTY, spa.FORECASTED_CONSUMPTION_QTY
+            FROM (
+                SELECT 
+                    pv.PROGRAM_ID, MAX(pv.VERSION_ID) MAX_VERSION 
+		FROM rm_program_version pv 
+		LEFT JOIN rm_program p ON pv.PROGRAM_ID=p.PROGRAM_ID
+		WHERE 
+                    TRUE 
+                    AND (@varApprovedSupplyPlanOnly=0 OR (@varApprovedSupplyPlanOnly=1 AND pv.VERSION_TYPE_ID=2 AND pv.VERSION_STATUS_ID=2))
+                    AND p.ACTIVE
+                    AND (LENGTH(@varProgramIds)=0 OR FIND_IN_SET(pv.PROGRAM_ID, @varProgramIds))
+                    AND (LENGTH(@varRealmCountryIds)=0 OR FIND_IN_SET(p.REALM_COUNTRY_ID, @varRealmCountryIds))
+		GROUP BY pv.PROGRAM_ID
+            ) p1
+            LEFT JOIN rm_supply_plan_amc spa ON p1.PROGRAM_ID=spa.PROGRAM_ID AND p1.MAX_VERSION=spa.VERSION_ID AND spa.TRANS_DATE BETWEEN @varStartDate AND @varStopDate
+            LEFT JOIN rm_planning_unit pu ON spa.PLANNING_UNIT_ID=pu.PLANNING_UNIT_ID
+            LEFT JOIN rm_forecasting_unit fu ON pu.FORECASTING_UNIT_ID=fu.FORECASTING_UNIT_ID
+            WHERE 
+                TRUE
+                AND spa.PLANNING_UNIT_ID IS NOT NULL
+				AND (LENGTH(@varPlanningUnitIds)=0 OR FIND_IN_SET(spa.PLANNING_UNIT_ID, @varPlanningUnitIds))
+				AND (LENGTH(@varTracerCategoryIds)=0 OR FIND_IN_SET(fu.TRACER_CATEGORY_ID, @varTracerCategoryIds));
+		
+    INSERT INTO tmp_forecastMetrics2 SELECT * FROM tmp_forecastMetrics1;
+        
     SET @sqlString = "";
     SET @sqlString = CONCAT(@sqlString, "SELECT ");
-	SET @sqlString = CONCAT(@sqlString, "    fm.TRANS_DATE, ");
+    SET @sqlString = CONCAT(@sqlString, "    fm.TRANS_DATE, ");
     SET @sqlString = CONCAT(@sqlString, "    p.PROGRAM_ID, p.PROGRAM_CODE, p.LABEL_ID `PROGRAM_LABEL_ID`, p.LABEL_EN `PROGRAM_LABEL_EN`, p.LABEL_FR `PROGRAM_LABEL_FR`, p.LABEL_SP `PROGRAM_LABEL_SP`, p.LABEL_PR `PROGRAM_LABEL_PR`, ");
     SET @sqlString = CONCAT(@sqlString, "    pu.PLANNING_UNIT_ID, pu.LABEL_ID `PLANNING_UNIT_LABEL_ID`, pu.LABEL_EN `PLANNING_UNIT_LABEL_EN`, pu.LABEL_FR `PLANNING_UNIT_LABEL_FR`, pu.LABEL_SP `PLANNING_UNIT_LABEL_SP`, pu.LABEL_PR  `PLANNING_UNIT_LABEL_PR`, ");
     SET @sqlString = CONCAT(@sqlString, "    fm.ACTUAL, fm.ACTUAL_CONSUMPTION_QTY `ACTUAL_CONSUMPTION`, fm.FORECASTED_CONSUMPTION_QTY `FORECASTED_CONSUMPTION`, ");
-	SET @sqlString = CONCAT(@sqlString, "	SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, fm2.ACTUAL_CONSUMPTION_QTY, null)) `ACTUAL_CONSUMPTION_TOTAL`, ");
-	SET @sqlString = CONCAT(@sqlString, "	SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, ABS(fm2.ACTUAL_CONSUMPTION_QTY-fm2.FORECASTED_CONSUMPTION_QTY), null)) `DIFF_CONSUMPTION_TOTAL`, ");
-	SET @sqlString = CONCAT(@sqlString, "	SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, 1, 0)) `MONTH_COUNT`, ");
-	SET @sqlString = CONCAT(@sqlString, "	SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, ABS(fm2.ACTUAL_CONSUMPTION_QTY-fm2.FORECASTED_CONSUMPTION_QTY), null))*100/SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, fm2.ACTUAL_CONSUMPTION_QTY, null)) `FORECAST_ERROR` ");
-	SET @sqlString = CONCAT(@sqlString, "FROM ",@tableName," fm ");
+    SET @sqlString = CONCAT(@sqlString, "	SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, fm2.ACTUAL_CONSUMPTION_QTY, null)) `ACTUAL_CONSUMPTION_TOTAL`, ");
+    SET @sqlString = CONCAT(@sqlString, "	SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, ABS(fm2.ACTUAL_CONSUMPTION_QTY-fm2.FORECASTED_CONSUMPTION_QTY), null)) `DIFF_CONSUMPTION_TOTAL`, ");
+    SET @sqlString = CONCAT(@sqlString, "	SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, 1, 0)) `MONTH_COUNT`, ");
+    SET @sqlString = CONCAT(@sqlString, "	SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, ABS(fm2.ACTUAL_CONSUMPTION_QTY-fm2.FORECASTED_CONSUMPTION_QTY), null))*100/SUM(IF(fm2.ACTUAL AND fm2.ACTUAL_CONSUMPTION_QTY IS NOT NULL AND fm2.FORECASTED_CONSUMPTION_QTY IS NOT NULL, fm2.ACTUAL_CONSUMPTION_QTY, null)) `FORECAST_ERROR` ");
+    SET @sqlString = CONCAT(@sqlString, "FROM tmp_forecastMetrics1 fm ");
     SET @sqlString = CONCAT(@sqlString, "LEFT JOIN vw_program p ON fm.PROGRAM_ID=p.PROGRAM_ID ");
     SET @sqlString = CONCAT(@sqlString, "LEFT JOIN vw_planning_unit pu ON fm.PLANNING_UNIT_ID=pu.PLANNING_UNIT_ID ");
-	SET @sqlString = CONCAT(@sqlString, "LEFT JOIN ",@tableName," fm2 ON fm.PROGRAM_ID=fm2.PROGRAM_ID AND fm.VERSION_ID=fm2.VERSION_ID AND fm.PLANNING_UNIT_ID=fm2.PLANNING_UNIT_ID ");
-	SET @sqlString = CONCAT(@sqlString, "WHERE fm.TRANS_DATE=@startDate ");
+    SET @sqlString = CONCAT(@sqlString, "LEFT JOIN tmp_forecastMetrics2 fm2 ON fm.PROGRAM_ID=fm2.PROGRAM_ID AND fm.VERSION_ID=fm2.VERSION_ID AND fm.PLANNING_UNIT_ID=fm2.PLANNING_UNIT_ID ");
+    SET @sqlString = CONCAT(@sqlString, "WHERE fm.TRANS_DATE=@varStopDate ");
     SET @sqlString = CONCAT(@sqlString, @aclSqlString);
-	SET @sqlString = CONCAT(@sqlString, "GROUP BY fm.PROGRAM_ID, fm.VERSION_ID, fm.PLANNING_UNIT_ID;");
+    SET @sqlString = CONCAT(@sqlString, "GROUP BY fm.PROGRAM_ID, fm.VERSION_ID, fm.PLANNING_UNIT_ID;");
+
     PREPARE S3 FROM @sqlString;
     EXECUTE S3;
-    SET @sqlString = "";
-    SET @sqlString = CONCAT(@sqlString, "DROP TABLE ",@tableName);
-    PREPARE S4 FROM @sqlString;
-    EXECUTE S4;
-
 END
