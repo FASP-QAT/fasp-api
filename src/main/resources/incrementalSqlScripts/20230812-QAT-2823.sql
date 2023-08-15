f21435c41958b5ac3d5473d418c020a94ca1bd52
dolly chhabriya <dolly.c@altius.cc>
2023-08-16 01:08:40 +0530

    Updated the view and update query

--- src/main/resources/incrementalSqlScripts/20230812-QAT-2823.sql
+++ src/main/resources/incrementalSqlScripts/20230812-QAT-2823.sql
@@ -20,4 +20,36 @@ ADD CONSTRAINT `fk_rm_forecast_tree_treeAnchorId`
 ALTER TABLE `fasp`.`rm_forecast_tree_anchor` ADD COLUMN `TREE_NAME` VARCHAR(255) NOT NULL AFTER `PROGRAM_ID`;
 
 INSERT INTO rm_forecast_tree_anchor SELECT NULL, ft.PROGRAM_ID, ft.LABEL_EN, MIN(ft.TREE_ID), MIN(ft.CREATED_DATE) FROM vw_forecast_tree ft group by ft.PROGRAM_ID, ft.LABEL_EN ORDER by ft.CREATED_DATE;
-UPDATE vw_forecast_tree t LEFT JOIN rm_forecast_tree_anchor ta ON t.PROGRAM_ID=ta.PROGRAM_ID AND ta.TREE_NAME=t.LABEL_EN SET t.TREE_ANCHOR_ID=ta.TREE_ANCHOR_ID;
+
+UPDATE rm_forecast_tree t 
+left join ap_label l on t.LABEL_ID=l.LABEL_ID
+LEFT JOIN rm_forecast_tree_anchor ta ON t.PROGRAM_ID=ta.PROGRAM_ID AND ta.TREE_NAME=l.LABEL_EN 
+SET t.TREE_ANCHOR_ID=ta.TREE_ANCHOR_ID;
+
+USE `fasp`;
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `faspUser`@`%` 
+    SQL SECURITY DEFINER
+VIEW `vw_forecast_tree` AS
+    SELECT 
+        `ft`.`TREE_ID` AS `TREE_ID`,
+        `ft`.`TREE_ANCHOR_ID` AS `TREE_ANCHOR_ID`,
+        `ft`.`PROGRAM_ID` AS `PROGRAM_ID`,
+        `ft`.`VERSION_ID` AS `VERSION_ID`,
+        `ft`.`LABEL_ID` AS `LABEL_ID`,
+        `ft`.`FORECAST_METHOD_ID` AS `FORECAST_METHOD_ID`,
+        `ft`.`CREATED_BY` AS `CREATED_BY`,
+        `ft`.`CREATED_DATE` AS `CREATED_DATE`,
+        `ft`.`LAST_MODIFIED_BY` AS `LAST_MODIFIED_BY`,
+        `ft`.`LAST_MODIFIED_DATE` AS `LAST_MODIFIED_DATE`,
+        `ft`.`ACTIVE` AS `ACTIVE`,
+        `l`.`LABEL_EN` AS `LABEL_EN`,
+        `l`.`LABEL_FR` AS `LABEL_FR`,
+        `l`.`LABEL_SP` AS `LABEL_SP`,
+        `l`.`LABEL_PR` AS `LABEL_PR`,
+        `ft`.`NOTES` AS `NOTES`
+    FROM
+        (`rm_forecast_tree` `ft`
+        LEFT JOIN `ap_label` `l` ON ((`ft`.`LABEL_ID` = `l`.`LABEL_ID`)));
+
