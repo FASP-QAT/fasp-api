a2f65528f4938932faeadcb07d00f8d7ce20ddfd
Akil Mahimwala <akil.m@altius.cc>
2023-11-08 15:20:55 +0530

    Updated the Reviewed Notes for bulk insert

--- src/main/java/cc/altius/FASP/dao/impl/ProgramDataDaoImpl.java
+++ src/main/java/cc/altius/FASP/dao/impl/ProgramDataDaoImpl.java
@@ -2009,7 +2009,7 @@ public class ProgramDataDaoImpl implements ProgramDataDao {
         String programVersionTransSql = "INSERT INTO rm_program_version_trans SELECT NULL,pv.PROGRAM_VERSION_ID,pv.VERSION_TYPE_ID,?,?,?,? FROM  rm_program_version pv  "
                 + "WHERE pv.`PROGRAM_ID`=? AND pv.`VERSION_ID`=? ";
         this.jdbcTemplate.update(programVersionTransSql, versionStatusId, notes, curUser.getUserId(), DateUtils.getCurrentDateObject(DateUtils.EST), programId, versionId);
-        String problemReportUpdateSql = "UPDATE rm_problem_report pr set pr.`REVIEW_NOTES`=:notes,pr.`REVIEWED_DATE`=IF(:reviewed,:curDate,pr.`REVIEWED_DATE`),pr.REVIEWED=:reviewed,pr.PROBLEM_STATUS_ID=:problemStatusId, pr.LAST_MODIFIED_BY=:curUser, pr.LAST_MODIFIED_DATE=:curDate WHERE pr.PROBLEM_REPORT_ID=:problemReportId";
+        String problemReportUpdateSql = "UPDATE rm_problem_report pr set pr.`REVIEW_NOTES`=:reviewedNotes, pr.`REVIEWED_DATE`=IF(:reviewed,:curDate,pr.`REVIEWED_DATE`),pr.REVIEWED=:reviewed,pr.PROBLEM_STATUS_ID=:problemStatusId, pr.LAST_MODIFIED_BY=:curUser, pr.LAST_MODIFIED_DATE=:curDate WHERE pr.PROBLEM_REPORT_ID=:problemReportId";
         String problemReportTransInsertSql = "INSERT INTO rm_problem_report_trans SELECT null, :problemReportId, :problemStatusId, :reviewed, :notes, :curUser, :curDate FROM rm_problem_report pr WHERE pr.PROBLEM_REPORT_ID=:problemReportId";
         SimpleJdbcInsert problemReportInsert = new SimpleJdbcInsert(this.dataSource).withTableName("rm_problem_report").usingGeneratedKeyColumns("PROBLEM_REPORT_ID");
         SimpleJdbcInsert problemReportTransInsert = new SimpleJdbcInsert(this.dataSource).withTableName("rm_problem_report_trans");
@@ -2021,7 +2021,7 @@ public class ProgramDataDaoImpl implements ProgramDataDao {
                 updateParams.put("reviewed", rp.isReviewed());
                 updateParams.put("curUser", curUser.getUserId());
                 updateParams.put("curDate", curDate);
-                updateParams.put("notes", rp.getNotes());
+                updateParams.put("reviewedNotes", rp.getReviewedNotes());
                 updateParams.put("problemStatusId", rp.getProblemStatus().getId());
                 updateParams.put("problemReportId", rp.getProblemReportId());
                 updateParamList.add(new MapSqlParameterSource(updateParams));
@@ -2034,7 +2034,7 @@ public class ProgramDataDaoImpl implements ProgramDataDao {
                 insertParams.put("PROBLEM_TYPE_ID", rp.getRealmProblem().getProblemType().getId());
                 insertParams.put("PROBLEM_STATUS_ID", rp.getProblemStatus().getId());
                 insertParams.put("REVIEWED", rp.isReviewed());
-                insertParams.put("REVIEWED_NOTES", (rp.isReviewed() ? rp.getNotes() : null));
+                insertParams.put("REVIEWED_NOTES", (rp.isReviewed() ? rp.getReviewedNotes() : null));
                 insertParams.put("REVIEWED_DATE", (rp.isReviewed() ? curDate : null));
                 insertParams.put("DATA1", rp.getDt());
                 insertParams.put("DATA2", rp.getRegion().getId());
