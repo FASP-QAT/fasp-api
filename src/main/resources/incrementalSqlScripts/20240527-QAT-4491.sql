9dc4bef30cf2eea8e6bc934468da3a1c72cbc389
dolly chhabriya <dolly.c@altius.cc>
2024-05-29 22:04:24 +0530

    Changed labels script

--- src/main/resources/incrementalSqlScripts/20240527-QAT-4491.sql
+++ src/main/resources/incrementalSqlScripts/20240527-QAT-4491.sql
@@ -15,10 +15,10 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,') está ultrapassando 
 INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.shipment.budgetWarning3','1'); 
 SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
 
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'(please see shipment ID');-- en
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'(veuillez consulter l'ID d'expédition');-- fr
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'(consulte el ID de envío');-- sp
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'(consulte o ID da remessa');-- pr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'if you submit.');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'si vous soumettez.');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'si lo envías.');-- sp
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'se você enviar.');-- pr
 INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.shipment.budgetWarning4','1'); 
 SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
 
