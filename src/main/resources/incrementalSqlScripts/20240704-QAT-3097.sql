333ac15c28ceef62d2f8536bb2022ca53d19db55
Rohit_Vishwakarma <rohit.v@altius.cc>
2024-07-08 17:08:45 +0530

    Added scripts

--- src/main/resources/incrementalSqlScripts/20240704-QAT-3097.sql
+++ src/main/resources/incrementalSqlScripts/20240704-QAT-3097.sql
@@ -1,3 +1,11 @@
+INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.program.dateRange','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Download specific range of supply plan data'); -- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Télécharger une gamme spécifique de données du plan d\'approvisionnement'); -- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Descargue una gama específica de datos del plan de suministro'); -- sp 
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Baixe uma gama específica de dados do plano de fornecimento'); -- pr
+
 USE `fasp`;
 DROP procedure IF EXISTS `getConsumptionDataNew`;
 
