c426830004f4eaaa35cbb7547dfbe7fe18402c35
Sonal Kothari <sonal.k@altius.cc>
2024-07-11 16:45:18 +0530

    labels added

--- src/main/resources/incrementalSqlScripts/20240705-QAT-4080.sql
+++ src/main/resources/incrementalSqlScripts/20240705-QAT-4080.sql
@@ -49,40 +49,72 @@ where l.LABEL_CODE='static.compareAndSelect.belowLowestConsumption' and ll.LANGU
 
 update ap_static_label l 
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
-set ll.LABEL_TEXT='Compare to Forecast'
+set ll.LABEL_TEXT='Compare to Consumption Forecast(s)'
 where l.LABEL_CODE='static.compareAndSelect.compareToConsumptionForecast' and ll.LANGUAGE_ID=1;
 
 update ap_static_label l 
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
-set ll.LABEL_TEXT='Comparar com a previsão'
+set ll.LABEL_TEXT='Compare com as previsões de consumo'
 where l.LABEL_CODE='static.compareAndSelect.compareToConsumptionForecast' and ll.LANGUAGE_ID=4;
 
 update ap_static_label l 
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
-set ll.LABEL_TEXT='Comparer aux prévisions'
+set ll.LABEL_TEXT='Comparer aux prévisions de consommation'
 where l.LABEL_CODE='static.compareAndSelect.compareToConsumptionForecast' and ll.LANGUAGE_ID=2;
 
 update ap_static_label l 
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
-set ll.LABEL_TEXT='Comparar con el pronóstico'
+set ll.LABEL_TEXT='Comparar con las previsiones de consumo'
 where l.LABEL_CODE='static.compareAndSelect.compareToConsumptionForecast' and ll.LANGUAGE_ID=3;
 
 update ap_static_label l 
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
-set ll.LABEL_TEXT='% above the highest forecast.'
+set ll.LABEL_TEXT='% above highest'
 where l.LABEL_CODE='static.compareAndSelect.aboveHighestConsumption' and ll.LANGUAGE_ID=1;
 
 update ap_static_label l 
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
-set ll.LABEL_TEXT='% acima da previsão mais alta.'
+set ll.LABEL_TEXT='% acima do mais alto'
 where l.LABEL_CODE='static.compareAndSelect.aboveHighestConsumption' and ll.LANGUAGE_ID=4;
 
 update ap_static_label l 
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
-set ll.LABEL_TEXT='% au-dessus de la prévision la plus élevée.'
+set ll.LABEL_TEXT='% au-dessus du plus haut'
 where l.LABEL_CODE='static.compareAndSelect.aboveHighestConsumption' and ll.LANGUAGE_ID=2;
 
 update ap_static_label l 
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
-set ll.LABEL_TEXT='% por encima de la previsión más alta.'
+set ll.LABEL_TEXT='% por encima del más alto'
 where l.LABEL_CODE='static.compareAndSelect.aboveHighestConsumption' and ll.LANGUAGE_ID=3;
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.compareAndSelect.selectForecast','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Click to select forecast.');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Cliquez pour sélectionner les prévisions.');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Clique para selecionar a previsão.');-- pr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Haga clic para seleccionar el pronóstico.');-- sp
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.compareAndSelect.forecastSelected','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Forecast selected.');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Prévision sélectionnée.');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Previsão selecionada.');-- pr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Pronóstico seleccionado.');-- sp
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.compareAndSelect.collapsePUPanel','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Click “-” to hide PU panel');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Cliquez sur « - » pour masquer le panneau PU');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Clique em “-” para ocultar o painel PU');-- pr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Haga clic en “-” para ocultar el panel PU');-- sp
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.compareAndSelect.expandPUPanel','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Expand PU panel');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Développer le panneau PU');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Expandir painel PU');-- pr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Ampliar panel de PU');-- sp
