ec374d17e90435793c494aa2e82d427a41c30751
Sonal Kothari <sonal.k@altius.cc>
2024-07-11 11:50:13 +0530

    labels added

--- src/main/resources/incrementalSqlScripts/20240613-QAT-4049.sql
+++ src/main/resources/incrementalSqlScripts/20240613-QAT-4049.sql
@@ -21,3 +21,11 @@ update ap_static_label l
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
 set ll.LABEL_TEXT='Exibir apenas o período de previsão'
 where l.LABEL_CODE='static.compareAndSelect.showOnlyForecastPeriod' and ll.LANGUAGE_ID=4;
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.extrapolations.showFits','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Display forecasts in months with actual consumption');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Afficher mensuellement les prévisions ainsi que les consommations réelles');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Exiba previsões em meses com consumo real');-- pr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Mostrar estimaciones en meses con consumo real');-- sp
