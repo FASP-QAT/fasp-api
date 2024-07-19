aff3b7f20d0bb6287a21cb0df65d3726ad03fae7
Pankaj Fate <pankaj.f@altius.cc>
2024-07-19 18:58:00 +0530

    sql for fst label added

--- src/main/resources/incrementalSqlScripts/20240716-QAT-2883.sql
+++ src/main/resources/incrementalSqlScripts/20240716-QAT-2883.sql
@@ -124,3 +124,16 @@ INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Par groupe');-- fr
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Agrupar por');-- pr
 
 INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Agrupar por');-- sp
+
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.funderType.funderTypeText','1');
+
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Funding Source Type is required');-- en
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Le type de source de financement est requis');-- fr
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'O tipo de fonte de financiamento é obrigatório');-- pr
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Se requiere tipo de fuente de financiamiento');-- sp
