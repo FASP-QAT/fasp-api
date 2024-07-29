0dfa90bfbb876c39f761f3f475d3f206b3df903a
Akil Mahimwala <akil.m@altius.cc>
2024-07-29 11:50:13 +0530

    Updated the error message that is sent

--- src/main/java/cc/altius/FASP/rest/controller/ExportApiRestController.java
+++ src/main/java/cc/altius/FASP/rest/controller/ExportApiRestController.java
@@ -86,13 +86,13 @@ public class ExportApiRestController {
                 throw new EmptyResultDataAccessException(1);
             }
         } catch (EmptyResultDataAccessException e) {
-            logger.error("Error while trying to list PlanningUnit for Program", e);
+            logger.error("Error while trying to get Export of Program", e);
             return new ResponseEntity(new ResponseCode("static.message.listFailed"), HttpStatus.NOT_FOUND);
         } catch (AccessDeniedException e) {
-            logger.error("Error while trying to list PlanningUnit for Program", e);
+            logger.error("Error while trying to get Export of Program", e);
             return new ResponseEntity(new ResponseCode("static.message.listFailed"), HttpStatus.FORBIDDEN);
         } catch (Exception e) {
-            logger.error("Error while trying to list PlanningUnit for Program", e);
+            logger.error("Error while trying to get Export of Program", e);
             return new ResponseEntity(new ResponseCode("static.message.listFailed"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
