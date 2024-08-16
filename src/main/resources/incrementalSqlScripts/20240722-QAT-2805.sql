f1e1c2374d97b48149068d71cb5295f17a14de31
Sonal Kothari <sonal.k@altius.cc>
2024-08-16 18:12:12 +0530

    label added

--- src/main/resources/incrementalSqlScripts/20240722-QAT-2805.sql
+++ src/main/resources/incrementalSqlScripts/20240722-QAT-2805.sql
@@ -17,3 +17,11 @@ update ap_static_label l
 left join ap_static_label_languages ll on l.STATIC_LABEL_ID=ll.STATIC_LABEL_ID
 set ll.LABEL_TEXT='Forecast Error calculated using Weighted Absolute Percentage Error (WAPE). The error displayed is calculated always based on the total forecast'
 where l.LABEL_CODE='static.tooltip.ForecastError' and ll.LANGUAGE_ID=1;
+
+INSERT INTO fasp.ap_static_label(STATIC_LABEL_ID,LABEL_CODE,ACTIVE) VALUES ( NULL,'static.compareAndSelect.noOfMonths','1');
+SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
+
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'Number of months included in calculation');-- en
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Nombre de mois inclus dans le calcul');-- fr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Número de meses incluídos no cálculo');-- pr
+INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Número de meses incluidos en el cálculo');-- sp
