148776f4e12d68a4b6d8f317b7f2285217a5fe7c
Rohit_Vishwakarma <rohit.v@altius.cc>
2024-11-02 12:16:24 +0530

    Added scripts

--- src/main/resources/incrementalSqlScripts/20241101-QAT-5326.sql
+++ src/main/resources/incrementalSqlScripts/20241101-QAT-5326.sql
@@ -1,2 +1,11 @@
 update ap_label al set al.LABEL_EN="Funnel"
 where al.LABEL_ID =(select nt.LABEL_ID from ap_node_type nt where nt.NODE_TYPE_ID=6);
+
+update ap_label al set al.LABEL_FR="Entonnoir"
+where al.LABEL_ID =(select nt.LABEL_ID from ap_node_type nt where nt.NODE_TYPE_ID=6);
+
+update ap_label al set al.LABEL_SP="Embudo"
+where al.LABEL_ID =(select nt.LABEL_ID from ap_node_type nt where nt.NODE_TYPE_ID=6);
+
+update ap_label al set al.LABEL_PR="Funil"
+where al.LABEL_ID =(select nt.LABEL_ID from ap_node_type nt where nt.NODE_TYPE_ID=6);
