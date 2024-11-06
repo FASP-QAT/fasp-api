bc4a8226a7069b2c4aa3fd292b20b28e0f065ce5
dolly chhabriya <dolly.c@altius.cc>
2024-11-06 12:05:41 +0530

    Removed duplicate label

--- src/main/resources/incrementalSqlScripts/20240424-QAT-4598.sql
+++ src/main/resources/incrementalSqlScripts/20240424-QAT-4598.sql
@@ -1,11 +1,3 @@
-INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.common.allitemsselected','1');
-SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
-
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,1,'All items are selected'); -- en
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,2,'Tous les éléments sont sélectionnés'); -- fr
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,3,'Todos los artículos están seleccionados'); -- sp 
-INSERT INTO ap_static_label_languages VALUES(NULL,@MAX,4,'Todos os itens são selecionados'); -- pr
-
 INSERT INTO `fasp`.`ap_static_label`(`STATIC_LABEL_ID`,`LABEL_CODE`,`ACTIVE`) VALUES ( NULL,'static.dataSet.forecastingProgramName','1');
 SELECT MAX(l.STATIC_LABEL_ID) INTO @MAX FROM ap_static_label l ;
 
