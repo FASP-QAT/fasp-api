3aec4c427bf59ffbb7c836533ffd11bb77f67be4
adanje <adanje@waersystems.info>
2024-12-06 10:15:33 +0200

    Fix issues raised on the Automated Testing PR
    Document the AbstractIntegrationTest so that we know it serves as a base class for integration tests as it provides database setup.
    Add TestCustomUserDetailsService to handle test specific authorisation.
    Remove test property file as it is no longer needed because we removed test authorisation logic from production code.
    Remove UserDaoImplTest as it heavily depend on mocks on jdbc template and sql queries, we better test this with integration tests

--- src/main/java/cc/altius/FASP/security/TestCustomUserDetailsService.java
+++ src/main/java/cc/altius/FASP/security/TestCustomUserDetailsService.java
@@ -1,50 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cc.altius.FASP.security;
 
 import cc.altius.FASP.dao.UserDao;
 import cc.altius.FASP.model.CustomUserDetails;
-import cc.altius.FASP.utils.LogUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
- *
- * @author akil
+ * Test-specific implementation of the CustomUserDetailsService.
  */
 @Service
-@Profile("!test")
-public class CustomUserDetailsService implements UserDetailsService {
+@Primary
+@Profile("test")
+public class TestCustomUserDetailsService extends CustomUserDetailsService {
 
-    private final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
-
+    private static final Logger logger = LoggerFactory.getLogger(TestCustomUserDetailsService.class);
     @Autowired
     private UserDao userDao;
-
     @Override
     public CustomUserDetails loadUserByUsername(String emailId) throws UsernameNotFoundException {
-        logger.info("Inside loadUserByUsername" + LogUtils.getArgsString(), LogUtils.getIpAddress(), LogUtils.getUsername());
+        logger.info("Test environment: Loading user by email {}", emailId);
         try {
-            CustomUserDetails user = this.userDao.getCustomUserByEmailId(emailId);
+            CustomUserDetails user = userDao.getCustomUserByEmailId(emailId);
+            if (user == null || !user.isPresent()) {
+                throw new UsernameNotFoundException("User not found");
+            }
+            user = userDao.getCustomUserByUserId(user.getUserId());
             if (user == null || !user.isPresent()) {
                 throw new UsernameNotFoundException("User not found");
             }
             return user;
-        } catch (NullPointerException ne) {
-            logger.warn("Error occurred", ne);
-            throw new UsernameNotFoundException(ne.getMessage());
         } catch (Exception e) {
             logger.warn("Error occurred", e);
             throw new UsernameNotFoundException(e.getMessage());
         }
     }
-
 }
