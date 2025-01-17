8ab5e3decb6a34a7ca12ca79ede57d71275da4ea
Dagmar Timler <dagmar.timler@gmail.com>
2025-01-17 16:15:14 +0200

    Finish Flyway implementation
    Finished the implementation for Flyway for the application itself. It
    is currently turned off (via the properties) but when it is enabled we
    can put our database scripts into the folder src/main/resources/db/migrate
    with a version prefix e.g. `V1__` and they will be automatically run
    in the application's database

--- src/test/java/cc/altius/FASP/common/TestFlywayConfig.java
+++ src/test/java/cc/altius/FASP/common/TestFlywayConfig.java
@@ -1,12 +1,8 @@
-package cc.altius.FASP.integration;
+package cc.altius.FASP.common;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.EventListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.flywaydb.core.Flyway;
 
 import javax.sql.DataSource;
@@ -15,30 +11,10 @@ import javax.sql.DataSource;
 @Profile("test")
 public class TestFlywayConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(TestFlywayConfig.class);
-
-    @Bean(initMethod = "migrate")
+    @Bean
     public Flyway flyway(DataSource dataSource) {
         return Flyway.configure()
             .dataSource(dataSource)
-            .baselineOnMigrate(true)
-            .baselineVersion("0")
-            .locations("classpath:db/migration", "classpath:db/testdata")
-            .cleanDisabled(false)
             .load();
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void migrateAndSeed(ApplicationReadyEvent event) {
-        log.info("Starting Flyway migration for tests...");
-        try {
-            // migrate
-            Flyway flyway = event.getApplicationContext().getBean(Flyway.class);
-            flyway.migrate();
-            log.info("Flyway migration completed successfully");
-        } catch (Exception e) {
-            log.error("Flyway migration failed", e);
-            throw e;
-        }
     }
 }
