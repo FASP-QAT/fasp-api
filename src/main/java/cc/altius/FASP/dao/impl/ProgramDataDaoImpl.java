0b5b8e4179fd4aa85a3786e4a83069b5d75afdd5
dolly chhabriya <dolly.c@altius.cc>
2025-09-05 13:02:28 +0530

    Changes for adding Integer.toString for target scenarion Id and added target scenario Id in hash and equals

--- src/main/java/cc/altius/FASP/dao/impl/ProgramDataDaoImpl.java
+++ src/main/java/cc/altius/FASP/dao/impl/ProgramDataDaoImpl.java
@@ -2296,7 +2296,7 @@ public class ProgramDataDaoImpl implements ProgramDataDao {
                         batchParams.put("TARGET_NODE_ID", getNewId(oldAndNewIdMap, "rm_forecast_tree_node", Integer.toString(dt.getTreeId()) + "-" + Integer.toString(n.getPayload().getNodeId())));
                         batchParams.put("SOURCE_TREE_ID", getNewId(oldAndNewIdMap, "rm_forecast_tree", Integer.toString(da.getTreeId())));
                         batchParams.put("SOURCE_SCENARIO_ID", getNewId(oldAndNewIdMap, "rm_scenario", da.getTreeId() + "-" + da.getScenarioId()));
-                        batchParams.put("TARGET_SCENARIO_ID", getNewId(oldAndNewIdMap, "rm_scenario", dt.getTreeId() + "-" + da.getTargetScenarioId()));
+                        batchParams.put("TARGET_SCENARIO_ID", getNewId(oldAndNewIdMap, "rm_scenario", Integer.toString(dt.getTreeId()) + "-" + Integer.toString(da.getTargetScenarioId())));
                         batchParams.put("SOURCE_NODE_ID", getNewId(oldAndNewIdMap, "rm_forecast_tree_node", da.getTreeId() + "-" + da.getNodeId()));
                         batchList.add(new MapSqlParameterSource(batchParams));
                     }
