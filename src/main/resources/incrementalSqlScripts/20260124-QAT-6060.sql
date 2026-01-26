8d137010e3b2683dd1b0a7fc8fa2a31ee402c761
dolly chhabriya <dolly.c@altius.cc>
2026-01-26 14:24:37 +0530

    Changes for hiding delinked shipment notification

--- src/main/resources/incrementalSqlScripts/20260124-QAT-6060.sql
+++ src/main/resources/incrementalSqlScripts/20260124-QAT-6060.sql
@@ -1,4 +1,9 @@
-CREATE DEFINER=`faspUser`@`localhost` PROCEDURE `getShipmentLinkingNotifications`(PROGRAM_ID INT(10), PLANNING_UNIT_ID TEXT, VERSION_ID INT (10))
+USE `fasp`;
+DROP procedure IF EXISTS `getShipmentLinkingNotifications`;
+
+DELIMITER $$
+USE `fasp`$$
+CREATE DEFINER=`faspUser`@`%` PROCEDURE `getShipmentLinkingNotifications`(PROGRAM_ID INT(10), VERSION_ID INT (10))
 BEGIN
     SET @programId = PROGRAM_ID;
     SET @procurementAgentId = 1;
@@ -14,7 +19,10 @@ LEFT JOIN vw_notification_type nt ON n.NOTIFICATION_TYPE_ID=nt.NOTIFICATION_TYPE
 left join rm_shipment_linking sl on sl.SHIPMENT_LINKING_ID=n.SHIPMENT_LINKING_ID
 left join rm_shipment_linking_trans slt on slt.SHIPMENT_LINKING_ID=sl.SHIPMENT_LINKING_ID and slt.VERSION_ID<=@versionId
 WHERE 
-	n.ACTIVE  AND sl.ACTIVE and sl.PROGRAM_ID=@programId and slt.VERSION_ID<=@versionId
+	n.ACTIVE AND sl.ACTIVE  and sl.PROGRAM_ID=@programId and slt.VERSION_ID<=@versionId
 GROUP BY n.NOTIFICATION_ID
 ORDER BY n.ADDRESSED DESC, n.NOTIFICATION_ID;
-END
+END$$
+
+DELIMITER ;
+;
